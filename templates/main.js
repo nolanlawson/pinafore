a249b2a608b84afc3fa9eee4cafcf1aad5ae5b21
Nolan Lawson <nolan@nolanlawson.com>
2018-01-07 23:00:15 -0800

    add URLSearchParams polyfill

--- templates/main.js
+++ templates/main.js
@@ -1,4 +1,17 @@
 import { init } from 'sapper/runtime.js';
 
-// `routes` is an array of route objects injected by Sapper
-init(document.querySelector('#sapper'), __routes__);
+// polyfills
+Promise.all([
+  typeof URLSearchParams === 'undefined' && import('url-search-params').then(Params => {
+    window.URLSearchParams = Params
+    Object.defineProperty(window.URL.prototype, 'searchParams', {
+      get() {
+        return new Params(this.search)
+      }
+    })
+  })
+]).then(() => {
+  console.log('done')
+  // `routes` is an array of route objects injected by Sapper
+  init(document.querySelector('#sapper'), __routes__)
+})
