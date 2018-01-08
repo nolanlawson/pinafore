6d90d44abc11b42da9de8a111ab7728636ff25c1
Nolan Lawson <nolan@nolanlawson.com>
2018-01-07 23:02:25 -0800

    cleanup

--- templates/main.js
+++ templates/main.js
@@ -2,7 +2,7 @@ import { init } from 'sapper/runtime.js';
 
 // polyfills
 Promise.all([
-  typeof URLSearchParams === 'undefined' && import('url-search-params').then(Params => {
+  typeof URLSearchParams === 'undefined' && import(/* webpackChunkName: 'url-search-params' */ 'url-search-params').then(Params => {
     window.URLSearchParams = Params
     Object.defineProperty(window.URL.prototype, 'searchParams', {
       get() {
@@ -11,7 +11,6 @@ Promise.all([
     })
   })
 ]).then(() => {
-  console.log('done')
   // `routes` is an array of route objects injected by Sapper
   init(document.querySelector('#sapper'), __routes__)
 })
