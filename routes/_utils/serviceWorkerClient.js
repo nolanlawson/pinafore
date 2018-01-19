b8d7ae3d8b692e4b5d11c1532751cdfe1c37a314
Nolan Lawson <nolan@nolanlawson.com>
2018-01-19 00:29:45 -0800

    tweak offline mode

--- routes/_utils/serviceWorkerClient.js
+++ routes/_utils/serviceWorkerClient.js
@@ -1,16 +1,11 @@
 import { toast } from './toast'
-import { keyval } from './database/keyval'
 
 function onUpdateFound(registration) {
   const newWorker = registration.installing
 
   newWorker.addEventListener('statechange', async () => {
-    if (!(await keyval.get('serviceworker_installed'))) {
-      await keyval.set('serviceworker_installed', true)
-      return
-    }
-    if (newWorker.state === 'activated' && navigator.serviceWorker.controller) {
-      toast.say('App update available. Reload to update.')
+    if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+      toast.say('Update available. Refresh to update.')
     }
   });
 }
