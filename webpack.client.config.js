a012bcfecdc4a837debbb524d811146cf5dc7fb2
Nolan Lawson <nolan@nolanlawson.com>
2018-01-19 17:17:24 -0800

    code cleanup

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,9 +1,8 @@
 const webpack = require('webpack')
 const config = require('sapper/webpack/config.js')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-var LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
 const isDev = config.dev;
 
