adfa5d5fb564c810e6af493e895fd2e41b93a9d8
Nolan Lawson <nolan@nolanlawson.com>
2018-01-20 19:19:24 -0800

    add modal dialog to fix fullscreen video

--- routes/_utils/asyncModules.js
+++ routes/_utils/asyncModules.js
@@ -1,3 +1,5 @@
+import { loadCSS } from 'fg-loadcss';
+
 const importURLSearchParams = () => import(
   /* webpackChunkName: 'url-search-params' */ 'url-search-params'
   ).then(Params => {
@@ -25,10 +27,25 @@ const importIndexedDBGetAllShim = () => import(
   /* webpackChunkName: 'indexeddb-getall-shim' */ 'indexeddb-getall-shim'
   )
 
+const importDialogPolyfill = (() => {
+  let cached
+  return () => {
+    if (cached) {
+      return Promise.resolve(cached)
+    }
+    loadCSS('/dialog-polyfill.css') // TODO: handle error
+    return import(/* webpackChunkName: 'dialog-polyfill' */ 'dialog-polyfill').then(res => {
+      cached = res
+      return cached
+    })
+  }
+})()
+
 export {
   importURLSearchParams,
   importTimeline,
   importIntersectionObserver,
   importRequestIdleCallback,
-  importIndexedDBGetAllShim
+  importIndexedDBGetAllShim,
+  importDialogPolyfill
 }
--- templates/main.js
+++ templates/main.js
@@ -6,7 +6,8 @@ import {
   importURLSearchParams,
   importIntersectionObserver,
   importRequestIdleCallback,
-  importIndexedDBGetAllShim
+  importIndexedDBGetAllShim,
+  importDialogPolyfill
 } from '../routes/_utils/asyncModules'
 
 // polyfills
@@ -14,7 +15,8 @@ Promise.all([
   typeof URLSearchParams === 'undefined' && importURLSearchParams(),
   typeof IntersectionObserver === 'undefined' && importIntersectionObserver(),
   typeof requestIdleCallback === 'undefined' && importRequestIdleCallback(),
-  !IDBObjectStore.prototype.getAll && importIndexedDBGetAllShim()
+  !IDBObjectStore.prototype.getAll && importIndexedDBGetAllShim(),
+  typeof HTMLDialogElement === 'undefined' && importDialogPolyfill()
 ]).then(() => {
   // `routes` is an array of route objects injected by Sapper
   init(document.querySelector('#sapper'), __routes__)
