a11f31bb3fc4dab61c0c833183ccd2d84b086e91
Nolan Lawson <nolan@nolanlawson.com>
2018-01-21 20:02:32 -0800

    implement hashtags

--- routes/_utils/mastodon/timelines.js
+++ routes/_utils/mastodon/timelines.js
@@ -1,9 +1,25 @@
 import { get, paramsString } from '../ajax'
 import { basename } from './utils'
 
+function getTimelineUrlName(timeline) {
+  switch (timeline) {
+    case 'local':
+    case 'federated':
+      return 'public'
+    case 'home':
+      return 'home'
+    default:
+      return 'tag'
+  }
+}
+
 export function getTimeline(instanceName, accessToken, timeline, maxId, since) {
-  let timelineUrlName = timeline === 'local' || timeline === 'federated' ?  'public' : timeline
+  let timelineUrlName = getTimelineUrlName(timeline)
   let url = `${basename(instanceName)}/api/v1/timelines/${timelineUrlName}`
+
+  if (timeline.startsWith('tag/')) {
+    url += '/' + timeline.split('/').slice(-1)[0]
+  }
 
   let params = {}
   if (since) {
