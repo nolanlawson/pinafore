8b64375b82474b0634a8710a2583f5b619424101
Nolan Lawson <nolan@nolanlawson.com>
2018-01-27 12:48:22 -0800

    build svgs automatically

---
+++ bin/build-svg.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const svgs = require('./svgs')
+const path = require('path')
+const fs = require('fs')
+const pify = require('pify')
+const SVGO = require('svgo')
+const svgo = new SVGO()
+const $ = require('cheerio')
+
+const readFile = pify(fs.readFile.bind(fs))
+const writeFile = pify(fs.writeFile.bind(fs))
+
+async function main() {
+  let result = (await Promise.all(svgs.map(async svg => {
+    let filepath = path.join(__dirname, '../', svg.src)
+    let content = await readFile(filepath, 'utf8')
+    let optimized = (await svgo.optimize(content))
+    let $optimized = $(optimized.data)
+    let $path = $optimized.find('path').removeAttr('fill')
+    let $symbol = $('<symbol></symbol>')
+      .attr('id', svg.id)
+      .attr('viewBox', `0 0 ${optimized.info.width} ${optimized.info.height}`)
+      .append($('<title>').val(svg.title))
+      .append($path)
+    return $.xml($symbol)
+  }))).join('\n')
+
+  result = `<svg xmlns="http://www.w3.org/2000/svg" style="display:none;">\n${result}\n</svg>`
+
+  let html2xxFilepath = path.join(__dirname, '../templates/2xx.html')
+  let html2xxFile = await readFile(html2xxFilepath, 'utf8')
+  html2xxFile = html2xxFile.replace(
+    /<!-- insert svg here -->[\s\S]+<!-- end insert svg here -->/,
+    '<!-- insert svg here -->' + result + '<!-- end insert svg here -->'
+  )
+  await writeFile(html2xxFilepath, html2xxFile, 'utf8')
+}
+
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
---
+++ bin/svgs.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {id: 'pinafore-logo', src: 'original-assets/sailboat.svg', title: 'Home'},
+  {id:'fa-bell', src:'node_modules/font-awesome-svg-png/white/svg/bell.svg', title: 'Notifications'},
+  {id:'fa-users', src:'node_modules/font-awesome-svg-png/white/svg/users.svg', title: 'Local'},
+  {id:'fa-globe', src:'node_modules/font-awesome-svg-png/white/svg/globe.svg', title: 'Federated'},
+  {id:'fa-gears', src:'node_modules/font-awesome-svg-png/white/svg/gears.svg', title: 'Settings'},
+  {id:'fa-reply', src:'node_modules/font-awesome-svg-png/white/svg/reply.svg', title: 'Reply'},
+  {id:'fa-retweet', src:'node_modules/font-awesome-svg-png/white/svg/retweet.svg', title: 'Boost'},
+  {id:'fa-star', src:'node_modules/font-awesome-svg-png/white/svg/star.svg', title: 'Favorite'},
+  {id:'fa-ellipsis-h', src:'node_modules/font-awesome-svg-png/white/svg/ellipsis-h.svg', title: 'More'},
+  {id:'fa-spinner', src:'node_modules/font-awesome-svg-png/white/svg/spinner.svg', title: 'Spinner'},
+  {id:'fa-user', src:'node_modules/font-awesome-svg-png/white/svg/user.svg', title: 'Empty user profile'},
+  {id:'fa-play-circle', src:'node_modules/font-awesome-svg-png/white/svg/play-circle.svg', title: 'Play'},
+  {id:'fa-eye', src:'node_modules/font-awesome-svg-png/white/svg/eye.svg', title: 'Show Sensitive Content'},
+  {id:'fa-eye-slash', src:'node_modules/font-awesome-svg-png/white/svg/eye-slash.svg', title: 'Hide Sensitive Content'},
+  {id:'fa-hashtag', src:'node_modules/font-awesome-svg-png/white/svg/hashtag.svg', title: 'Hashtag'},
+]
