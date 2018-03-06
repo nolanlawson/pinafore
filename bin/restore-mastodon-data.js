c574606f0c5dd8d73a256b34ace74497ba0c7b9c
Nolan Lawson <nolan@nolanlawson.com>
2018-03-05 23:46:50 -0800

    fix media uploads in restoration script

--- bin/restore-mastodon-data.js
+++ bin/restore-mastodon-data.js
@@ -1,7 +1,6 @@
 import { actions } from './mastodon-data'
 import { users } from '../tests/users'
 import { pinStatus, postStatus } from '../routes/_api/statuses'
-import { uploadMedia } from '../routes/_api/media'
 import { followAccount } from '../routes/_api/follow'
 import { favoriteStatus } from '../routes/_api/favorite'
 import { reblogStatus } from '../routes/_api/reblog'
@@ -9,16 +8,39 @@ import fetch from 'node-fetch'
 import FileApi from 'file-api'
 import path from 'path'
 import fs from 'fs'
-import pify from 'pify'
-
-const readFile = pify(fs.readFile.bind(fs))
+import FormData from 'form-data'
+import { auth } from '../routes/_api/utils'
 
 global.File = FileApi.File
 global.FormData = FileApi.FormData
 global.fetch = fetch
 
+async function submitMedia(accessToken, filename, alt) {
+  let form = new FormData()
+  form.append('file', fs.createReadStream(path.join(__dirname, '../tests/images/' + filename)))
+  form.append('description', alt)
+  return new Promise((resolve, reject) => {
+    form.submit({
+      host: 'localhost',
+      port: 3000,
+      path: '/api/v1/media',
+      headers: auth(accessToken)
+    }, (err, res) => {
+      if (err) {
+        return reject(err)
+      }
+      let data = ''
+
+      res.on('data', chunk => {
+        data += chunk
+      })
+
+      res.on('end', () => resolve(JSON.parse(data)))
+    })
+  })
+}
+
 export async function restoreMastodonData () {
-  debugger
   console.log('Restoring mastodon data...')
   let internalIdsToIds = {}
   for (let action of actions) {
@@ -30,14 +52,7 @@ export async function restoreMastodonData () {
         inReplyTo = internalIdsToIds[inReplyTo]
       }
       let mediaIds = media && await Promise.all(media.map(async mediaItem => {
-        let type = mediaItem.endsWith('gif') ? 'image/gif'
-          : mediaItem.endsWith('jpg') ? 'image/jpg' : 'video/mp4'
-        let file = new File({
-          name: mediaItem,
-          type: type,
-          buffer: await readFile(path.join(__dirname, '../tests/images/' + mediaItem))
-        })
-        let mediaResponse = await uploadMedia('localhost:3000', accessToken, file, 'kitten')
+        let mediaResponse = await submitMedia(accessToken, mediaItem, 'kitten')
         return mediaResponse.id
       }))
       let status = await postStatus('localhost:3000', accessToken, text, inReplyTo, mediaIds,
