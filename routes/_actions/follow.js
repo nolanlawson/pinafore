92176df3ab4a6d87d1816fc06c4a58cf4a4d1e25
Nolan Lawson <nolan@nolanlawson.com>
2018-03-11 15:22:31 -0700

    add ability to follow/unfollow an account

---
+++ routes/_actions/follow.js
@@ -0,0 +1,22 @@
+import { store } from '../_store/store'
+import { followAccount, unfollowAccount } from '../_api/follow'
+import { database } from '../_database/database'
+import { toast } from '../_utils/toast'
+
+export async function setAccountFollowed (accountId, follow) {
+  let instanceName = store.get('currentInstance')
+  let accessToken = store.get('accessToken')
+  try {
+    if (follow) {
+      await followAccount(instanceName, accessToken, accountId)
+    } else {
+      await unfollowAccount(instanceName, accessToken, accountId)
+    }
+    let relationship = await database.getRelationship(instanceName, accountId)
+    relationship.following = follow
+    await database.setRelationship(instanceName, relationship)
+  } catch (e) {
+    console.error(e)
+    toast.say(`Unable to ${follow ? 'follow' : 'unfollow'} account: ` + (e.message || ''))
+  }
+}
