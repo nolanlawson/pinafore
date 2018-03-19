a1b0f3221d0761b319e9b3abe97cab49f396050e
Nolan Lawson <nolan@nolanlawson.com>
2018-03-18 17:54:35 -0700

    almost working except for a bug on add.html

--- webpack/service-worker.config.js
+++ webpack/service-worker.config.js
@@ -1,7 +1,11 @@
 const config = require('sapper/webpack/config.js')
 
+const mode = process.env.NODE_ENV
+const isDev = mode === 'development'
+
 module.exports = {
   entry: config.serviceworker.entry(),
   output: config.serviceworker.output(),
-  mode: process.env.NODE_ENV
+  mode: process.env.NODE_ENV,
+  devtool: isDev ? 'inline-source-map' : 'source-map'
 }
