72e7e18e0bbe4ae6b2979f43fa2076e29bd5d575
Nolan Lawson <nolan@nolanlawson.com>
2018-03-21 09:38:20 -0700

    add favorite/reblog animations

---
+++ routes/_static/animations.js
@@ -0,0 +1,25 @@
+export const FAVORITE_ANIMATION = [
+  {
+    properties: [
+      {transform: 'scale(1)'},
+      {transform: 'scale(2)'},
+      {transform: 'scale(1)'}
+    ],
+    options: {
+      duration: 400,
+      easing: 'ease-in-out'
+    }
+  },
+  {
+    properties: [
+      {fill: 'var(--action-button-fill-color)'},
+      {fill: 'var(--action-button-fill-color-pressed)'}
+    ],
+    options: {
+      duration: 400,
+      easing: 'linear'
+    }
+  }
+]
+
+export const REBLOG_ANIMATION = FAVORITE_ANIMATION
--- routes/_utils/loadPolyfills.js
+++ routes/_utils/loadPolyfills.js
@@ -1,13 +1,15 @@
 import {
   importIntersectionObserver,
   importRequestIdleCallback,
-  importIndexedDBGetAllShim
+  importIndexedDBGetAllShim,
+  importWebAnimationPolyfill
 } from './asyncModules'
 
 export function loadPolyfills () {
   return Promise.all([
     typeof IntersectionObserver === 'undefined' && importIntersectionObserver(),
     typeof requestIdleCallback === 'undefined' && importRequestIdleCallback(),
-    !IDBObjectStore.prototype.getAll && importIndexedDBGetAllShim()
+    !IDBObjectStore.prototype.getAll && importIndexedDBGetAllShim(),
+    !Element.prototype.animate && importWebAnimationPolyfill()
   ])
 }
