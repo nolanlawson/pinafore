6f350b68d3a7cb110bd7a8490448822482e704c6
Nolan Lawson <nolan@nolanlawson.com>
2018-03-28 18:37:53 -0700

    fix content warning should delete when you hide it

--- routes/_actions/contentWarnings.js
+++ routes/_actions/contentWarnings.js
@@ -2,5 +2,9 @@ import { store } from '../_store/store'
 
 export function toggleContentWarningShown (realm) {
   let shown = store.getComposeData(realm, 'contentWarningShown')
-  store.setComposeData(realm, {contentWarningShown: !shown})
+  let contentWarning = store.getComposeData(realm, 'contentWarning')
+  store.setComposeData(realm, {
+    contentWarning: shown ? contentWarning : '',
+    contentWarningShown: !shown
+  })
 }
