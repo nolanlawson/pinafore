07794367145701a110bf43f918992eeb920a59ba
Nolan Lawson <nolan@nolanlawson.com>
2018-03-31 17:26:10 -0700

    fix service worker webpack assets not being found

--- templates/main.js
+++ templates/main.js
@@ -4,6 +4,10 @@ import '../routes/_utils/serviceWorkerClient'
 import '../routes/_utils/historyEvents'
 import '../routes/_utils/loadingMask'
 
+console.log('1')
+console.log('2')
+console.log('3')
+
 loadPolyfills().then(() => {
   console.log('init()')
   // `routes` is an array of route objects injected by Sapper
