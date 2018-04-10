36af90506f640e68c9ade0c3eacb06e8a9fa9661
Nolan Lawson <nolan@nolanlawson.com>
2018-04-10 16:58:17 -0700

    travis: fix postgres import

--- bin/run-mastodon.js
+++ bin/run-mastodon.js
@@ -21,6 +21,7 @@ const envFile = `
 PAPERCLIP_SECRET=foo
 SECRET_KEY_BASE=bar
 OTP_SECRET=foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar
+DB_HOST=127.0.0.1
 DB_PORT=${process.env.PGPORT || 5432}
 DB_USER=${DB_USER}
 `
@@ -43,12 +44,12 @@ async function cloneMastodon () {
 async function setupMastodonDatabase () {
   console.log('Setting up mastodon database...')
   try {
-    await exec(`dropdb -U ${DB_USER} mastodon_development`, {cwd: mastodonDir})
+    await exec(`dropdb -h 127.0.0.1 -U ${DB_USER} mastodon_development`, {cwd: mastodonDir})
   } catch (e) { /* ignore */ }
-  await exec(`createdb -U ${DB_USER} mastodon_development`, {cwd: mastodonDir})
+  await exec(`createdb -h 127.0.0.1 -U ${DB_USER} mastodon_development`, {cwd: mastodonDir})
 
   let dumpFile = path.join(dir, '../fixtures/dump.sql')
-  await exec(`pg_restore -U ${DB_USER} -Fc -d mastodon_development "${dumpFile}"`, {cwd: mastodonDir})
+  await exec(`pg_restore -h 127.0.0.1 -U ${DB_USER} -Fc -d mastodon_development "${dumpFile}"`, {cwd: mastodonDir})
 
   let tgzFile = path.join(dir, '../fixtures/system.tgz')
   let systemDir = path.join(mastodonDir, 'public/system')
