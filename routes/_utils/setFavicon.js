95c3349db74bdfea7191306a7bf14a04bbc64b19
Nolan Lawson <nolan@nolanlawson.com>
2018-04-11 20:30:38 -0700

    fix bug where favicon changes incorrectly

--- routes/_utils/setFavicon.js
+++ routes/_utils/setFavicon.js
@@ -1,14 +1,17 @@
 // borrowed from https://github.com/HenrikJoreteg/favicon-setter
 export function setFavicon (href) {
   let faviconId = 'theFavicon'
-  let link = document.createElement('link')
   let oldLink = document.getElementById(faviconId)
+
+  if (oldLink.getAttribute('href') === href) {
+    return
+  }
+
+  let link = document.createElement('link')
   link.id = faviconId
   link.rel = 'shortcut icon'
   link.type = 'image/png'
   link.href = href
-  if (oldLink) {
-    document.head.removeChild(oldLink)
-  }
+  document.head.removeChild(oldLink)
   document.head.appendChild(link)
 }
