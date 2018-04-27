7a3469083d36756503bcc3a43a9a355d3fe45290
Nolan Lawson <nolan@nolanlawson.com>
2018-04-26 22:05:55 -0700

    add test and aria-label

---
+++ tests/spec/021-followers-follows.js
@@ -0,0 +1,26 @@
+import {
+  followersButton,
+  followsButton, getNthSearchResult,
+  getNthStatus, getUrl, goBack
+} from '../utils'
+import { foobarRole } from '../roles'
+
+fixture`021-followers-follows.js`
+  .page`http://localhost:4002`
+
+test('shows followers and follows', async t => {
+  await t.useRole(foobarRole)
+    .click(getNthStatus(0).find('.status-author-name'))
+    .expect(getUrl()).match(/\/accounts\/3$/)
+    .expect(followsButton.getAttribute('aria-label')).eql('Follows 2')
+    .click(followsButton)
+    .expect(getUrl()).contains('/accounts/3/follows')
+    .expect(getNthSearchResult(1).innerText).contains('@foobar')
+    .expect(getNthSearchResult(2).innerText).contains('@admin')
+  await goBack()
+  await t.expect(getUrl()).match(/\/accounts\/3$/)
+    .expect(followersButton.getAttribute('aria-label')).eql('Followed by 1')
+    .click(followersButton)
+    .expect(getUrl()).contains('/accounts/3/followers')
+    .expect(getNthSearchResult(1).innerText).contains('@admin')
+})
