6217e4df7cc75ba18d47a168780949c9d13875d0
Nolan Lawson <nolan@nolanlawson.com>
2018-04-29 19:51:44 -0700

    fix stuff

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -26,7 +26,6 @@ module.exports = {
             dev: isDev,
             hydratable: true,
             emitCss: !isDev,
-            hot: isDev,
             hotReload: isDev
           }
         }
