8425130856a406250f7f2085acaa5edf02308aa2
Nolan Lawson <nolan@nolanlawson.com>
2018-05-27 09:40:09 -0700

    explicitly install ruby 2.5.0 in travis

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -7,6 +7,10 @@ if [[ "$COMMAND" = deploy-dev-travis ]]; then
   exit 0 # no need to setup mastodon in this case
 fi
 
+source "$HOME/.rvm/scripts/rvm"
+rvm install 2.5.0
+rvm use 2.5.0
+
 sudo -E add-apt-repository -y ppa:mc3man/trusty-media
 sudo -E apt-get update
 sudo -E apt-get install -y ffmpeg
