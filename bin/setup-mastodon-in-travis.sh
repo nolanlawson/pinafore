9c50e256c8bc7fec5b4b584a73a4888af1982b2e
Nolan Lawson <nolan@nolanlawson.com>
2018-06-07 21:33:19 -0700

    only install deps when running mastodon in travis

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -12,7 +12,15 @@ rvm use 2.5.1
 
 sudo -E add-apt-repository -y ppa:mc3man/trusty-media
 sudo -E apt-get update
-sudo -E apt-get install -y ffmpeg
+sudo -E apt-get install -y \
+  autoconf bison build-essential ffmpeg file \
+  g++ gcc imagemagick libffi-dev libgdbm-dev \
+  libgdbm3 libicu-dev libidn11-dev libncurses5-dev \
+  libpq-dev libprotobuf-dev libreadline6-dev libssl-dev \
+  libxml2-dev libxslt1-dev libyaml-dev nodejs \
+  pkg-config postgresql-10 postgresql-client-10 \
+  postgresql-contrib-10 protobuf-compiler redis-tools \
+  zlib1g-dev
 ruby --version
 node --version
 npm --version
