314de7f5fdd2d4874a2f9dcf005e0836a7ab77e6
Nolan Lawson <nolan@nolanlawson.com>
2018-08-22 19:08:36 -0700

    more work on img.decode

---
+++ routes/_store/observers/resizeObservers.js
@@ -0,0 +1,14 @@
+import { registerResizeListener } from '../../_utils/resize'
+
+export function resizeObservers (store) {
+  if (!process.browser) {
+    return
+  }
+
+  const recalculateIsMobileSize = () => {
+    store.set({isMobileSize: window.matchMedia('(max-width: 767px)').matches})
+  }
+
+  registerResizeListener(recalculateIsMobileSize)
+  recalculateIsMobileSize()
+}
