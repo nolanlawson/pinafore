3e5a80159ef8814cc80c3e9ea4f6c1d7657263de
Nolan Lawson <nolan@nolanlawson.com>
2018-08-26 09:32:54 -0700

    fix incorrect toast notification for follow requests

--- routes/_actions/follow.js
+++ routes/_actions/follow.js
@@ -2,28 +2,19 @@ import { store } from '../_store/store'
 import { followAccount, unfollowAccount } from '../_api/follow'
 import { toast } from '../_utils/toast'
 import { updateProfileAndRelationship } from './accounts'
-import {
-  getRelationship as getRelationshipFromDatabase
-} from '../_database/relationships'
 
 export async function setAccountFollowed (accountId, follow, toastOnSuccess) {
   let { currentInstance, accessToken } = store.get()
   try {
-    let account
     if (follow) {
-      account = await followAccount(currentInstance, accessToken, accountId)
+      await followAccount(currentInstance, accessToken, accountId)
     } else {
-      account = await unfollowAccount(currentInstance, accessToken, accountId)
+      await unfollowAccount(currentInstance, accessToken, accountId)
     }
     await updateProfileAndRelationship(accountId)
-    let relationship = await getRelationshipFromDatabase(currentInstance, accountId)
     if (toastOnSuccess) {
       if (follow) {
-        if (account.locked && relationship.requested) {
-          toast.say('Requested to follow account')
-        } else {
-          toast.say('Followed account')
-        }
+        toast.say('Followed account')
       } else {
         toast.say('Unfollowed account')
       }
