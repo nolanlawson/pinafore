cbf91f076ff64e2c487271804aa735374b0d9c00
Nolan Lawson <nolan@nolanlawson.com>
2018-09-23 10:11:40 -0700

    add a theme preview

--- routes/_static/themes.js
+++ routes/_static/themes.js
@@ -1,55 +1,81 @@
 const themes = [
   {
     name: 'default',
-    label: 'Royal (default)'
+    label: 'Royal (default)',
+    dark: false,
+    color: 'royalblue'
   },
   {
     name: 'scarlet',
-    label: 'Scarlet'
+    label: 'Scarlet',
+    dark: false,
+    color: '#e04e41'
   },
   {
     name: 'seafoam',
-    label: 'Seafoam'
+    label: 'Seafoam',
+    dark: false,
+    color: '#177380'
   },
   {
     name: 'hotpants',
-    label: 'Hotpants'
+    label: 'Hotpants',
+    dark: false,
+    color: 'hotpink'
   },
   {
     name: 'oaken',
-    label: 'Oaken'
+    label: 'Oaken',
+    dark: false,
+    color: 'saddlebrown'
   },
   {
     name: 'majesty',
-    label: 'Majesty'
+    label: 'Majesty',
+    dark: false,
+    color: 'blueviolet'
   },
   {
     name: 'gecko',
-    label: 'Gecko'
+    label: 'Gecko',
+    dark: false,
+    color: '#4ab92f'
   },
   {
     name: 'ozark',
-    label: 'Ozark'
+    label: 'Ozark',
+    dark: true,
+    color: '#5263af'
   },
   {
     name: 'cobalt',
-    label: 'Cobalt'
+    label: 'Cobalt',
+    dark: true,
+    color: '#08439b'
   },
   {
     name: 'sorcery',
-    label: 'Sorcery'
+    label: 'Sorcery',
+    dark: true,
+    color: '#ae91e8'
   },
   {
     name: 'punk',
-    label: 'Punk'
+    label: 'Punk',
+    dark: true,
+    color: '#e04e41'
   },
   {
     name: 'riot',
-    label: 'Riot'
+    label: 'Riot',
+    dark: true,
+    color: 'hotpink'
   },
   {
     name: 'hacker',
-    label: 'Hacker'
+    label: 'Hacker',
+    dark: true,
+    color: '#4ab92f'
   }
 ]
 
