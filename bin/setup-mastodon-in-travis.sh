924885e532beabeba1917fcaf407bdacade9f42e
Nolan Lawson <nolan@nolanlawson.com>
2018-11-05 08:58:26 -0800

    fix redis issue in travis without building redis from source (#619)

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -11,13 +11,12 @@ source "$HOME/.rvm/scripts/rvm"
 rvm install 2.5.1
 rvm use 2.5.1
 
-# build redis from source until this is fixed: https://git.io/fxjkb
-curl -O http://download.redis.io/redis-stable.tar.gz
-tar -xzf redis-stable.tar.gz
-cd redis-stable
-make
-sudo make install
-redis-server --daemonize yes
+# fix for redis IPv6 issue
+# https://travis-ci.community/t/trusty-environment-redis-server-not-starting-with-redis-tools-installed/650/2
+sudo sed -e 's/^bind.*/bind 127.0.0.1/' /etc/redis/redis.conf > redis.conf
+sudo mv redis.conf /etc/redis
+sudo service redis-server start
+echo PING | nc localhost 6379 # check redis running
 
 # install ffmpeg from PPA because it's not in Trusty
 sudo -E add-apt-repository -y ppa:mc3man/trusty-media
