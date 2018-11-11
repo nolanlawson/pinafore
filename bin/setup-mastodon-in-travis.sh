03e0afff6df76a018bc3f43eca282128b8cd013c
Nolan Lawson <nolan@nolanlawson.com>
2018-11-11 14:14:39 -0800

    chore(travis): try explicit apt-get installs for faster scripts

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -11,17 +11,50 @@ source "$HOME/.rvm/scripts/rvm"
 rvm install 2.5.1
 rvm use 2.5.1
 
+# equivalent to addons: postgresql: 10
+sudo -E travis_setup_postgresql 10
+
+# install ffmpeg from PPA because it's not in Trusty
+sudo -E add-apt-repository -y ppa:mc3man/trusty-media
+sudo -E apt-get update
+sudo -E apt-get install \
+  -yq --no-install-suggests --no-install-recommends \
+  autoconf \
+  bison \
+  build-essential \
+  file \
+  ffmpeg \
+  g++ \
+  gcc \
+  imagemagick \
+  libffi-dev \
+  libgdbm-dev \
+  libgdbm3 \
+  libicu-dev \
+  libidn11-dev \
+  libncurses5-dev \
+  libpq-dev \
+  libprotobuf-dev \
+  libreadline6-dev \
+  libssl-dev \
+  libxml2-dev \
+  libxslt1-dev \
+  libyaml-dev \
+  pkg-config nodejs \
+  postgresql-10 \
+  postgresql-client-10 \
+  postgresql-contrib-10 \
+  protobuf-compiler \
+  redis-server \
+  redis-tools \
+  zlib1g-dev
+
 # fix for redis IPv6 issue
 # https://travis-ci.community/t/trusty-environment-redis-server-not-starting-with-redis-tools-installed/650/2
 sudo sed -e 's/^bind.*/bind 127.0.0.1/' /etc/redis/redis.conf > redis.conf
 sudo mv redis.conf /etc/redis
 sudo service redis-server start
 echo PING | nc localhost 6379 # check redis running
-
-# install ffmpeg from PPA because it's not in Trusty
-sudo -E add-apt-repository -y ppa:mc3man/trusty-media
-sudo -E apt-get update
-sudo -E apt-get install -y ffmpeg
 
 # check versions
 ruby --version
