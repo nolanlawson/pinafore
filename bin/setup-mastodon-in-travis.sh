b0bfc5908f3a4690e5b6b254e665344c7fdb08b2
Nolan Lawson <nolan@nolanlawson.com>
2018-11-11 14:17:44 -0800

    fixup

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -11,9 +11,6 @@ source "$HOME/.rvm/scripts/rvm"
 rvm install 2.5.1
 rvm use 2.5.1
 
-# equivalent to addons: postgresql: 10
-sudo -E travis_setup_postgresql 10
-
 # install ffmpeg from PPA because it's not in Trusty
 sudo -E add-apt-repository -y ppa:mc3man/trusty-media
 sudo -E apt-get update
