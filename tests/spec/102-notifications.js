d8811930d128f472ad9ec16af25ae95bc97e8593
Nolan Lawson <nolan@nolanlawson.com>
2018-11-12 17:34:51 -0800

    feat(title): add dynamic document title
    fixes #490 and #643

--- tests/spec/102-notifications.js
+++ tests/spec/102-notifications.js
@@ -1,6 +1,6 @@
 import { loginAsFoobar } from '../roles'
 import {
-  getNthStatus, getUrl, homeNavButton, notificationsNavButton
+  getNthStatus, getTitleText, getUrl, homeNavButton, notificationsNavButton
 } from '../utils'
 import { favoriteStatusAs, postAs } from '../serverActions'
 
@@ -12,15 +12,19 @@ test('shows unread notifications', async t => {
   await loginAsFoobar(t)
   await t
     .expect(notificationsNavButton.getAttribute('aria-label')).eql('Notifications')
+    .expect(getTitleText()).eql('localhost:3000 · Home')
   await favoriteStatusAs('admin', id)
   await t
     .expect(notificationsNavButton.getAttribute('aria-label')).eql('Notifications (1)')
+    .expect(getTitleText()).eql('localhost:3000 · Home (1)')
     .click(notificationsNavButton)
     .expect(getUrl()).contains('/notifications')
     .expect(notificationsNavButton.getAttribute('aria-label')).eql('Notifications (current page)')
+    .expect(getTitleText()).eql('localhost:3000 · Notifications')
     .expect(getNthStatus(0).innerText).contains('somebody please favorite this to validate me')
     .expect(getNthStatus(0).innerText).match(/admin\s+favorited your status/)
   await t
     .click(homeNavButton)
     .expect(notificationsNavButton.getAttribute('aria-label')).eql('Notifications')
+    .expect(getTitleText()).eql('localhost:3000 · Home')
 })
