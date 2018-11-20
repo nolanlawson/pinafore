508f42c6a55e8625f88eb325d9b160c6cffba24a
Nolan Lawson <nolan@nolanlawson.com>
2018-11-20 07:53:13 -0800

    fix(scroll): fix offsetHeight for scrolling whole document

--- routes/_utils/scrollContainer.js
+++ routes/_utils/scrollContainer.js
@@ -9,3 +9,11 @@ export function addScrollListener (listener) {
 export function removeScrollListener (listener) {
   document.removeEventListener('scroll', listener)
 }
+
+export function getOffsetHeight () {
+  // in a subscroller, this would be element.offsetHeight, but here
+  // document.scrollingElement.offsetHeight is too short for some reason.
+  // This one is exact, such that scrollHeight - scrollTop - offsetHeight === 0
+  // when you are scrolled to the bottom.
+  return window.innerHeight
+}
