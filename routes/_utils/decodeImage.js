8d2901717f89441de3a56cb34200aed69b317f53
Nolan Lawson <nolan@nolanlawson.com>
2018-12-03 21:12:23 -0800

    fix(firefox): fix firefox with blocked images

--- routes/_utils/decodeImage.js
+++ routes/_utils/decodeImage.js
@@ -1,10 +1,18 @@
-export function decodeImage (img) {
+import { mark, stop } from './marks'
+
+async function doDecodeImage (img) {
   if (typeof img.decode === 'function') {
     return img.decode()
   }
-
   return new Promise((resolve, reject) => {
     img.onload = resolve
     img.onerror = reject
   })
+}
+
+export async function decodeImage (img) {
+  mark('decodeImage')
+  let res = await doDecodeImage(img)
+  stop('decodeImage')
+  return res
 }
