cd5f607d00404585ddf5a8ef2a5a53757066e001
Nolan Lawson <nolan@nolanlawson.com>
2018-12-03 21:18:51 -0800

    remove excessive perf marks

--- routes/_utils/decodeImage.js
+++ routes/_utils/decodeImage.js
@@ -1,18 +1,10 @@
-import { mark, stop } from './marks'
-
-async function doDecodeImage (img) {
+export function decodeImage (img) {
   if (typeof img.decode === 'function') {
     return img.decode()
   }
+
   return new Promise((resolve, reject) => {
     img.onload = resolve
     img.onerror = reject
   })
-}
-
-export async function decodeImage (img) {
-  mark('decodeImage')
-  let res = await doDecodeImage(img)
-  stop('decodeImage')
-  return res
 }
