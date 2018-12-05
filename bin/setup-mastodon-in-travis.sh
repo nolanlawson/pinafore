e12eb468a352c4f46d2acc65d13dee9e2c15fb43
Nolan Lawson <nolan@nolanlawson.com>
2018-12-04 23:02:43 -0800

    chore(travis): use travis container without sudo

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -11,13 +11,6 @@ source "$HOME/.rvm/scripts/rvm"
 rvm install 2.5.3
 rvm use 2.5.3
 
-# fix for redis IPv6 issue
-# https://travis-ci.community/t/trusty-environment-redis-server-not-starting-with-redis-tools-installed/650/2
-sudo sed -e 's/^bind.*/bind 127.0.0.1/' /etc/redis/redis.conf > redis.conf
-sudo mv redis.conf /etc/redis
-sudo service redis-server start
-echo PING | nc localhost 6379 # check redis running
-
 # install ffmpeg because it's not in Trusty
 if [ ! -f /home/travis/ffmpeg-static/ffmpeg ]; then
   rm -fr /home/travis/ffmpeg-static
@@ -28,8 +21,6 @@ if [ ! -f /home/travis/ffmpeg-static/ffmpeg ]; then
     'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz'
   tar -x -C /home/travis/ffmpeg-static --strip-components 1 -f ffmpeg.tar.xz --wildcards '*/ffmpeg' --wildcards '*/ffprobe'
 fi
-sudo ln -s /home/travis/ffmpeg-static/ffmpeg /usr/local/bin/ffmpeg
-sudo ln -s /home/travis/ffmpeg-static/ffprobe /usr/local/bin/ffprobe
 
 # check versions
 ruby --version
