9d150711c3fc1e99762a4bcfe9dc813c8ae037f5
Nolan Lawson <nolan@nolanlawson.com>
2018-12-04 22:52:33 -0800

    chore(travis): test if redis can work without hacks now

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -11,11 +11,6 @@ source "$HOME/.rvm/scripts/rvm"
 rvm install 2.5.3
 rvm use 2.5.3
 
-# fix for redis IPv6 issue
-# https://travis-ci.community/t/trusty-environment-redis-server-not-starting-with-redis-tools-installed/650/2
-sudo sed -e 's/^bind.*/bind 127.0.0.1/' /etc/redis/redis.conf > redis.conf
-sudo mv redis.conf /etc/redis
-sudo service redis-server start
 echo PING | nc localhost 6379 # check redis running
 
 # install ffmpeg because it's not in Trusty
