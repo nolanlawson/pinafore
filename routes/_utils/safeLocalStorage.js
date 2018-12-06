3ca1519f265efe1303e7190795139382e812da6c
Nolan Lawson <nolan@nolanlawson.com>
2018-12-05 20:22:21 -0800

    just check for indexeddb

--- routes/_utils/safeLocalStorage.js
+++ routes/_utils/safeLocalStorage.js
@@ -1,11 +1,6 @@
 import localStorageMemory from 'localstorage-memory'
+import { testHasLocalStorage } from './testStorage'
 
-let safeLocalStorage
-
-try {
-  safeLocalStorage = process.browser && localStorage // safari throws here if cookies are disabled
-} catch (e) {
-  safeLocalStorage = localStorageMemory
-}
+const safeLocalStorage = testHasLocalStorage() ? localStorage : localStorageMemory
 
 export { safeLocalStorage }
