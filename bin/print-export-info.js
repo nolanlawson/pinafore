db85148a4ccc4054de7814b50f3b84ecf134810e
Nolan Lawson <nolan@nolanlawson.com>
2018-12-10 23:35:33 -0800

    add better csp info

--- bin/print-export-info.js
+++ bin/print-export-info.js
@@ -5,6 +5,11 @@ const checksum = require('../inline-script-checksum').checksum
 const html = fs.readFileSync(path.join(__dirname, '../__sapper__/export/index.html'), 'utf8')
 const nonce = html.match(/<script nonce=([^>]+)>/)[1]
 
+const csp = `add_header Content-Security-Policy "script-src 'self' 'sha256-${checksum}' 'nonce-${nonce}'; ` +
+`worker-src 'self'; style-src 'self' 'unsafe-inline'; frame-src 'none'; object-src 'none'; manifest-src 'self';`
+
+fs.writeFileSync(path.join(__dirname, '../__sapper__/export/.csp.nginx'), csp, 'utf8')
+
 console.log(`                                                                                             
 
                ,((*
@@ -31,9 +36,13 @@ Export successful! Static files are in:
 
     __sapper__/export/
 
-Serve, and add this CSP header to your nginx config:
+Be sure to add the CSP header to your nginx config:
 
-    add_header Content-Security-Policy "script-src 'self' 'sha256-${checksum}' 'nonce-${nonce}'; worker-src 'self'; style-src 'self' 'unsafe-inline'; frame-src 'none'; object-src 'none'; manifest-src 'self'"
+    server {
+        include ${path.resolve(__dirname, '..')}/__sapper__/export/.csp.nginx;
+    }
+
+This file will be updated whenever you do \`npm run export\`.
 
 Enjoy Pinafore!
 `)
