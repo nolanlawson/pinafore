83f2bfd703f890b73356d0a379c7349203a0046d
Nolan Lawson <nolan@nolanlawson.com>
2018-12-24 11:28:17 -0800

    fix test

--- tests/spec/102-notifications.js
+++ tests/spec/102-notifications.js
@@ -15,8 +15,8 @@ test('shows unread notifications', async t => {
     .expect(getTitleText()).eql('localhost:3000 · Home')
   await favoriteStatusAs('admin', id)
   await t
-    .expect(notificationsNavButton.getAttribute('aria-label')).eql('Notifications (1)')
-    .expect(getTitleText()).eql('localhost:3000 · Home (1)')
+    .expect(notificationsNavButton.getAttribute('aria-label')).eql('(1) Notifications')
+    .expect(getTitleText()).eql('(1) localhost:3000 · Home')
     .click(notificationsNavButton)
     .expect(getUrl()).contains('/notifications')
     .expect(notificationsNavButton.getAttribute('aria-label')).eql('Notifications (current page)')
