a84a41861eab66227553644225b71306a29cb59a
Nolan Lawson <nolan@nolanlawson.com>
2019-01-13 14:51:07 -0800

    feat: date title shows absolute date
    fixes #759

--- src/routes/_utils/formatters.js
+++ src/routes/_utils/formatters.js
@@ -1,29 +1,15 @@
-let dateFormatter
+export const absoluteDateFormatter = new Intl.DateTimeFormat('en-US', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit'
+})
 
-export function getDateFormatter () {
-  if (!dateFormatter) {
-    dateFormatter = new Intl.DateTimeFormat('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
-    })
-  }
-  return dateFormatter
-}
-
-let shortDateFormatter
-
-export function getShortDateFormatter () {
-  if (!shortDateFormatter) {
-    shortDateFormatter = new Intl.DateTimeFormat('en-US', {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
-    })
-  }
-  return shortDateFormatter
-}
+export const shortAbsoluteDateFormatter = new Intl.DateTimeFormat('en-US', {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit'
+})
