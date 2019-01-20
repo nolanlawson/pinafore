6ef2b323e051f07c429900bbfc4c2e0c6b73cecc
Nolan Lawson <nolan@nolanlawson.com>
2019-01-19 22:55:23 -0800

    fix: fix notification m/p keyboard shortcuts
    fixes #905

--- tests/spec/026-shortcuts-notification.js
+++ tests/spec/026-shortcuts-notification.js
@@ -56,3 +56,33 @@ test('Shortcut m toggles mention in a follow notification', async t => {
     .click(closeDialogButton)
     .expect(modalDialog.exists).notOk()
 })
+
+test('Shortcut p refers to booster in a boost notification', async t => {
+  let idx = 1 // "@admin boosted your status"
+  await loginAsFoobar(t)
+  await t
+    .expect(getUrl()).eql('http://localhost:4002/')
+    .click(notificationsNavButton)
+    .expect(getUrl()).contains('/notifications')
+    .expect(getNthStatus(0).exists).ok({ timeout: 30000 })
+    .pressKey('j '.repeat(idx + 1))
+    .expect(getNthStatus(idx).hasClass('status-active')).ok()
+    .pressKey('p')
+    .expect(getUrl()).contains('/accounts/1')
+})
+
+test('Shortcut m refers to favoriter in a favorite notification', async t => {
+  let idx = 0 // "@admin favorited your status"
+  await loginAsFoobar(t)
+  await t
+    .expect(getUrl()).eql('http://localhost:4002/')
+    .click(notificationsNavButton)
+    .expect(getUrl()).contains('/notifications')
+    .expect(getNthStatus(0).exists).ok({ timeout: 30000 })
+    .pressKey('j '.repeat(idx + 1))
+    .expect(getNthStatus(idx).hasClass('status-active')).ok()
+    .pressKey('m')
+    .expect(composeModalInput.value).eql('@admin ')
+    .click(closeDialogButton)
+    .expect(modalDialog.exists).notOk()
+})
