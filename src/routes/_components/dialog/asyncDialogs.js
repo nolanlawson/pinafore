92cb528c59a58054ab62c96498808c46a0323a92
Nolan Lawson <nolan@nolanlawson.com>
2019-02-02 14:08:47 -0800

    feat: add carousel for media modal

--- src/routes/_components/dialog/asyncDialogs.js
+++ src/routes/_components/dialog/asyncDialogs.js
@@ -1,39 +1,37 @@
+const getDefault = mod => mod.default
+
 export const importShowAccountProfileOptionsDialog = () => import(
   /* webpackChunkName: 'showAccountProfileOptionsDialog' */ './creators/showAccountProfileOptionsDialog'
-  ).then(mod => mod.default)
+  ).then(getDefault)
 
 export const importShowComposeDialog = () => import(
   /* webpackChunkName: 'showComposeDialog' */ './creators/showComposeDialog'
-  ).then(mod => mod.default)
+  ).then(getDefault)
 
 export const importShowConfirmationDialog = () => import(
   /* webpackChunkName: 'showConfirmationDialog' */ './creators/showConfirmationDialog'
-  ).then(mod => mod.default)
+  ).then(getDefault)
 
 export const importShowEmojiDialog = () => import(
   /* webpackChunkName: 'showEmojiDialog' */ './creators/showEmojiDialog'
-  ).then(mod => mod.default)
-
-export const importShowImageDialog = () => import(
-  /* webpackChunkName: 'showImageDialog' */ './creators/showImageDialog'
-  ).then(mod => mod.default)
+  ).then(getDefault)
 
 export const importShowPostPrivacyDialog = () => import(
   /* webpackChunkName: 'showPostPrivacyDialog' */ './creators/showPostPrivacyDialog'
-  ).then(mod => mod.default)
+  ).then(getDefault)
 
 export const importShowStatusOptionsDialog = () => import(
   /* webpackChunkName: 'showStatusOptionsDialog' */ './creators/showStatusOptionsDialog'
-  ).then(mod => mod.default)
-
-export const importShowVideoDialog = () => import(
-  /* webpackChunkName: 'showVideoDialog' */ './creators/showVideoDialog'
-  ).then(mod => mod.default)
+  ).then(getDefault)
 
 export const importShowCopyDialog = () => import(
   /* webpackChunkName: 'showCopyDialog' */ './creators/showCopyDialog'
-  ).then(mod => mod.default)
+  ).then(getDefault)
 
 export const importShowShortcutHelpDialog = () => import(
   /* webpackChunkName: 'showShortcutHelpDialog' */ './creators/showShortcutHelpDialog'
-  ).then(mod => mod.default)
+  ).then(getDefault)
+
+export const importShowMediaDialog = () => import(
+  /* webpackChunkName: 'showMediaDialog' */ './creators/showMediaDialog'
+  ).then(getDefault)
