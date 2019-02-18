ce20e2b3ae4fed56e3d50662b0262a83a14dc4cc
Nolan Lawson <nolan@nolanlawson.com>
2019-02-18 11:31:38 -0800

    perf: use lite web animations polyfill

--- src/routes/_utils/loadPolyfills.js
+++ src/routes/_utils/loadPolyfills.js
@@ -2,15 +2,13 @@ import {
   importCustomElementsPolyfill,
   importIndexedDBGetAllShim,
   importIntersectionObserver,
-  importRequestIdleCallback,
-  importWebAnimationPolyfill
+  importRequestIdleCallback
 } from './asyncPolyfills'
 
 export function loadPolyfills () {
   return Promise.all([
     typeof IntersectionObserver === 'undefined' && importIntersectionObserver(),
     typeof requestIdleCallback === 'undefined' && importRequestIdleCallback(),
-    !Element.prototype.animate && importWebAnimationPolyfill(),
     !IDBObjectStore.prototype.getAll && importIndexedDBGetAllShim(),
     typeof customElements === 'undefined' && importCustomElementsPolyfill()
   ])
