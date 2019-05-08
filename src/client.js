85dce0fbd4eb07455e68de6d1873e2bf4450da1d
Nolan Lawson <nolan@nolanlawson.com>
2019-05-07 23:45:34 -0700

    fixup

--- src/client.js
+++ src/client.js
@@ -4,18 +4,12 @@ import './routes/_utils/serviceWorkerClient'
 import './routes/_utils/historyEvents'
 import './routes/_utils/loadingMask'
 
-import { snackbar } from './routes/_components/snackbar/snackbar'
-
 loadPolyfills().then(() => {
   console.log('init()')
   sapper.start({ target: document.querySelector('#sapper') })
 })
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
-
-setTimeout(() => {
-  snackbar.announce('App update available.', 'Refresh')
-}, 2000)
 
 if (module.hot) {
   module.hot.accept()
