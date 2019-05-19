13908630fe70d83905c04bb39b0f4e443e99d896
Nolan Lawson <nolan@nolanlawson.com>
2019-05-19 08:11:30 -0700

    fix: separate "inline theme" from "default theme"

--- src/routes/_utils/themeEngine.js
+++ src/routes/_utils/themeEngine.js
@@ -2,8 +2,9 @@ let meta = process.browser && document.getElementById('theThemeColor')
 let offlineStyle = process.browser && document.getElementById('theOfflineStyle')
 let prefersDarkTheme = process.browser && window.matchMedia('(prefers-color-scheme: dark)').matches
 
-export const DEFAULT_LIGHT_THEME = 'default'
-export const DEFAULT_DARK_THEME = 'ozark'
+export const INLINE_THEME = 'default' // theme that does not require external CSS
+export const DEFAULT_LIGHT_THEME = 'default' // theme that is shown by default
+export const DEFAULT_DARK_THEME = 'ozark' // theme that is shown for prefers-color-scheme:dark
 export const DEFAULT_THEME = prefersDarkTheme ? DEFAULT_DARK_THEME : DEFAULT_LIGHT_THEME
 
 function getExistingThemeLink () {
@@ -38,7 +39,7 @@ function loadCSS (href) {
 export function switchToTheme (themeName = DEFAULT_THEME) {
   let themeColor = window.__themeColors[themeName]
   meta.content = themeColor || window.__themeColors[DEFAULT_THEME]
-  if (themeName !== DEFAULT_LIGHT_THEME) {
+  if (themeName !== INLINE_THEME) {
     loadCSS(`/theme-${themeName}.css`)
   } else {
     resetExistingTheme()
