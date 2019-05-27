369b42723e63bfdd185e064afefcd5b5b712c66e
Nolan Lawson <nolan@nolanlawson.com>
2019-05-26 23:55:06 -0700

    fix: fix reduceMotion of svelte slide transition
    fixes #1236

---
+++ src/routes/_transitions/slide.js
@@ -0,0 +1,17 @@
+import { slide as svelteSlide } from 'svelte-transitions'
+import { store } from '../_store/store'
+import noop from 'lodash-es/noop'
+
+// same as svelte-transitions, but respecting reduceMotion
+export function slide (node, ref) {
+  let { reduceMotion } = store.get()
+  if (reduceMotion) {
+    return {
+      delay: 0,
+      duration: 1, // setting to 0 causes some kind of built-in duration
+      easing: _ => _,
+      css: noop
+    }
+  }
+  return svelteSlide(node, ref)
+}
