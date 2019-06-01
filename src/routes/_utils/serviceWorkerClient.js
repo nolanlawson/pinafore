74230cfe8e256d0b259833ba43978e821c930def
Nolan Lawson <nolan@nolanlawson.com>
2019-06-01 13:07:38 -0700

    fix: fix service worker for real (#1258)
    fixes #1243

--- src/routes/_utils/serviceWorkerClient.js
+++ src/routes/_utils/serviceWorkerClient.js
@@ -1,11 +1,22 @@
 import { snackbar } from '../_components/snackbar/snackbar'
 
+async function skipWaiting () {
+  const reg = await navigator.serviceWorker.getRegistration()
+  if (!reg || !reg.waiting) {
+    return
+  }
+  reg.waiting.postMessage('skip-waiting')
+}
+
 function onUpdateFound (registration) {
   const newWorker = registration.installing
 
   newWorker.addEventListener('statechange', async () => {
     if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-      snackbar.announce('App update available.', 'Reload', () => document.location.reload(true))
+      snackbar.announce('App update available.', 'Reload', async () => {
+        await skipWaiting()
+        document.location.reload(true)
+      })
     }
   })
 }
