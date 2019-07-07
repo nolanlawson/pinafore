804b757c5bb212d35f47de93bf13fab59699f48c
Nolan Lawson <nolan@nolanlawson.com>
2019-07-07 16:49:02 -0700

    fix postgres

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -11,6 +11,9 @@ source "$HOME/.rvm/scripts/rvm"
 rvm install 2.6.1
 rvm use 2.6.1
 
+# setup postgres user in Travis
+psql -d template1 -c "CREATE USER pinafore WITH PASSWORD 'pinafore' CREATEDB;" -U postgres
+
 echo PING | nc localhost 6379 # check redis running
 
 # check versions
