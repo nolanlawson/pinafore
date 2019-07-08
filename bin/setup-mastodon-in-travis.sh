cfc182a83686b565a7d4ed8cb6e96255a0dc7e8f
Nolan Lawson <nolan@nolanlawson.com>
2019-07-07 17:39:20 -0700

    fix: fix ffmpeg install in travis (#1318)

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -18,19 +18,6 @@ sudo mv redis.conf /etc/redis
 sudo service redis-server start
 echo PING | nc localhost 6379 # check redis running
 
-# install ffmpeg because it's not in Trusty
-if [ ! -f /home/travis/ffmpeg-static/ffmpeg ]; then
-  rm -fr /home/travis/ffmpeg-static
-  mkdir -p /home/travis/ffmpeg-static
-  curl -sL \
-    -A 'https://github.com/nolanlawson/pinafore' \
-    -o ffmpeg.tar.xz \
-    'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz'
-  tar -x -C /home/travis/ffmpeg-static --strip-components 1 -f ffmpeg.tar.xz --wildcards '*/ffmpeg' --wildcards '*/ffprobe'
-fi
-sudo ln -s /home/travis/ffmpeg-static/ffmpeg /usr/local/bin/ffmpeg
-sudo ln -s /home/travis/ffmpeg-static/ffprobe /usr/local/bin/ffprobe
-
 # check versions
 ruby --version
 node --version
