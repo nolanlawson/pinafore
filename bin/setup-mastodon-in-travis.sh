d91607bbcdee9b0d0760c09587343361d39cafa4
Nolan Lawson <nolan@nolanlawson.com>
2019-07-07 17:22:47 -0700

    try using older postgres

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -11,9 +11,6 @@ source "$HOME/.rvm/scripts/rvm"
 rvm install 2.6.1
 rvm use 2.6.1
 
-# setup postgres user in Travis
-psql -d template1 -h 127.0.0.1 -p 5433 -c -U postgres "CREATE USER pinafore WITH PASSWORD 'pinafore' CREATEDB;"
-
 echo PING | nc localhost 6379 # check redis running
 
 # check versions
