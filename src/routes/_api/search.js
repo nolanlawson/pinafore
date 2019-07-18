015fe0080cd6ae5d3e4cd813b798d253ccc2e5c3
Nolan Lawson <nolan@nolanlawson.com>
2019-07-17 21:34:37 -0700

    fix: limit search results from 40 to 5 (#1336)
    40 is too much, also this aligns us with the Mastodon frontend UI.

--- src/routes/_api/search.js
+++ src/routes/_api/search.js
@@ -1,7 +1,7 @@
 import { get, paramsString, DEFAULT_TIMEOUT } from '../_utils/ajax'
 import { auth, basename } from './utils'
 
-export function search (instanceName, accessToken, query, resolve = true, limit = 40, signal = null) {
+export function search (instanceName, accessToken, query, resolve = true, limit = 5, signal = null) {
   let url = `${basename(instanceName)}/api/v1/search?` + paramsString({
     q: query,
     resolve,
