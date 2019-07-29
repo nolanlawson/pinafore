59bdd0bb77bc6a406482cb56d06b4fdf3ee13761
Nolan Lawson <nolan@nolanlawson.com>
2019-07-28 19:38:34 -0700

    fix: large streaming gaps by refetching (#1362)
    fixes #1356

--- src/routes/_actions/stream/fillStreamingGap.js
+++ src/routes/_actions/stream/fillStreamingGap.js
@@ -1,15 +1,23 @@
-// TODO: should probably just keep fetching timeline items in the gap, not stop at 40
 import { addStatusesOrNotifications } from '../addStatusOrNotification'
 import { getTimeline } from '../../_api/timelines'
 
-const TIMELINE_GAP_BATCH_SIZE = 40
+const TIMELINE_GAP_BATCH_SIZE = 20 // Mastodon timeline API maximum limit
+const MAX_NUM_REQUESTS = 15 // to avoid getting caught in an infinite loop somehow
 
 // fill in the "streaming gap" – i.e. fetch the most recent items so that there isn't
 // a big gap in the timeline if you haven't looked at it in awhile
 export async function fillStreamingGap (instanceName, accessToken, timelineName, firstTimelineItemId) {
-  let newTimelineItems = await getTimeline(instanceName, accessToken,
-    timelineName, null, firstTimelineItemId, TIMELINE_GAP_BATCH_SIZE)
-  if (newTimelineItems.length) {
-    addStatusesOrNotifications(instanceName, timelineName, newTimelineItems)
-  }
+  let maxId = null
+  let numRequests = 0
+  let newTimelineItems
+
+  do {
+    numRequests++
+    newTimelineItems = await getTimeline(instanceName, accessToken,
+      timelineName, maxId, firstTimelineItemId, TIMELINE_GAP_BATCH_SIZE)
+    if (newTimelineItems.length) {
+      addStatusesOrNotifications(instanceName, timelineName, newTimelineItems)
+      maxId = newTimelineItems[newTimelineItems.length - 1].id
+    }
+  } while (numRequests < MAX_NUM_REQUESTS && newTimelineItems.length === TIMELINE_GAP_BATCH_SIZE)
 }
