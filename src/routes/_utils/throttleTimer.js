8e0221231e579f9b7f85a8cb164474fdc8e3fa4a
Nolan Lawson <nolan@nolanlawson.com>
2019-08-07 19:20:16 -0700

    consistent syntax

--- src/routes/_utils/throttleTimer.js
+++ src/routes/_utils/throttleTimer.js
@@ -2,7 +2,7 @@
 // It's tedious to do this using cancelAnimationFrame, so this is a utility to throttle
 // a timer such that it only runs the last callback when it fires.
 
-export const throttleTimer = (timer) => {
+export const throttleTimer = timer => {
   let queuedCallback
 
   const flush = () => {
