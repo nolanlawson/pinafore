dcb0c692a1c791ea26619dd4a8b87d151b7352ab
Sorin Davidoi <sorin.davidoi@protonmail.com>
2019-08-09 17:42:23 +0200

    refactor(utils/blurhash): Use constant for number of pixels

--- src/routes/_utils/blurhash.js
+++ src/routes/_utils/blurhash.js
@@ -1,17 +1,18 @@
 import { decode as decodeBlurHash } from 'blurhash'
 import { mark, stop } from './marks'
 
+const RESOLUTION = 32
 let canvas
 
 export function decode (blurhash) {
   mark('computeBlurhash')
-  const pixels = decodeBlurHash(blurhash, 32, 32)
+  const pixels = decodeBlurHash(blurhash, RESOLUTION, RESOLUTION)
 
   if (pixels) {
     canvas = canvas || document.createElement('canvas')
-    canvas.height = 32
-    canvas.width = 32
-    const imageData = new window.ImageData(pixels, 32, 32)
+    canvas.height = RESOLUTION
+    canvas.width = RESOLUTION
+    const imageData = new window.ImageData(pixels, RESOLUTION, RESOLUTION)
     canvas.getContext('2d').putImageData(imageData, 0, 0)
     const base64Image = canvas.toDataURL()
     stop('computeBlurhash')
