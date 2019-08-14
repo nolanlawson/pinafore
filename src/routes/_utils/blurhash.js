7af9278c98edc1fad53b7d94156c40515c090df7
Sorin Davidoi <sorin.davidoi@protonmail.com>
2019-08-15 00:14:00 +0300

    refactor: Use toBlob, block media rendering until blurhash ready

--- src/routes/_utils/blurhash.js
+++ src/routes/_utils/blurhash.js
@@ -1,25 +1,27 @@
 import { decode as decodeBlurHash } from 'blurhash'
-import { mark, stop } from './marks'
 
 const RESOLUTION = 32
 let canvas
 
-export function decode (blurhash) {
-  mark('computeBlurhash')
-  try {
-    const pixels = decodeBlurHash(blurhash, RESOLUTION, RESOLUTION)
+export async function decode (blurhash) {
+  return new Promise((resolve, reject) => {
+    try {
+      const pixels = decodeBlurHash(blurhash, RESOLUTION, RESOLUTION)
 
-    if (pixels) {
-      if (!canvas) {
-        canvas = canvas || document.createElement('canvas')
-        canvas.height = RESOLUTION
-        canvas.width = RESOLUTION
+      if (pixels) {
+        if (!canvas) {
+          canvas = canvas || document.createElement('canvas')
+          canvas.height = RESOLUTION
+          canvas.width = RESOLUTION
+        }
+        const imageData = new ImageData(pixels, RESOLUTION, RESOLUTION)
+        canvas.getContext('2d').putImageData(imageData, 0, 0)
+        canvas.toBlob(blob => {
+          resolve(URL.createObjectURL(blob))
+        })
       }
-      const imageData = new ImageData(pixels, RESOLUTION, RESOLUTION)
-      canvas.getContext('2d').putImageData(imageData, 0, 0)
-      return canvas.toDataURL()
+    } catch (e) {
+      reject(e)
     }
-  } finally {
-    stop('computeBlurhash')
-  }
+  })
 }
