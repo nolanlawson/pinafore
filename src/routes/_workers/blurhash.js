fa00e046271819d5ad70a654b8c15cb04f60cf4a
Sorin Davidoi <sorin.davidoi@protonmail.com>
2019-08-15 12:23:03 +0300

    fix(workers/blurhash): Don't resolve promise twice

--- src/routes/_workers/blurhash.js
+++ src/routes/_workers/blurhash.js
@@ -16,28 +16,28 @@ self.addEventListener('message', ({ data: { encoded } }) => {
       } else {
         postMessage({ encoded, imageData: CACHE.get(encoded), decoded: null, error: null })
       }
-    }
-
-    const pixels = decodeBlurHash(encoded, RESOLUTION, RESOLUTION)
-
-    if (pixels) {
-      const imageData = new ImageData(pixels, RESOLUTION, RESOLUTION)
-
-      if (OFFSCREEN_CANVAS) {
-        OFFSCREEN_CANVAS_CONTEXT_2D.putImageData(imageData, 0, 0)
-        OFFSCREEN_CANVAS.convertToBlob().then(blob => {
-          const decoded = URL.createObjectURL(blob)
-          CACHE.set(encoded, decoded)
-          postMessage({ encoded, decoded, imageData: null, error: null })
-        }).catch(error => {
-          postMessage({ encoded, decoded: null, imageData: null, error })
-        })
-      } else {
-        CACHE.set(encoded, imageData)
-        postMessage({ encoded, imageData, decoded: null, error: null })
-      }
     } else {
-      postMessage({ encoded, decoded: null, imageData: null, error: new Error('decode did not return any pixels') })
+      const pixels = decodeBlurHash(encoded, RESOLUTION, RESOLUTION)
+
+      if (pixels) {
+        const imageData = new ImageData(pixels, RESOLUTION, RESOLUTION)
+
+        if (OFFSCREEN_CANVAS) {
+          OFFSCREEN_CANVAS_CONTEXT_2D.putImageData(imageData, 0, 0)
+          OFFSCREEN_CANVAS.convertToBlob().then(blob => {
+            const decoded = URL.createObjectURL(blob)
+            CACHE.set(encoded, decoded)
+            postMessage({ encoded, decoded, imageData: null, error: null })
+          }).catch(error => {
+            postMessage({ encoded, decoded: null, imageData: null, error })
+          })
+        } else {
+          CACHE.set(encoded, imageData)
+          postMessage({ encoded, imageData, decoded: null, error: null })
+        }
+      } else {
+        postMessage({ encoded, decoded: null, imageData: null, error: new Error('decode did not return any pixels') })
+      }
     }
   } catch (error) {
     postMessage({ encoded, decoded: null, imageData: null, error })
