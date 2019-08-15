66c829aea3a801f11a60466c3758395f9b5eeda2
Sorin Davidoi <sorin.davidoi@protonmail.com>
2019-08-15 09:25:32 +0300

    feat(workers/blurhash): Use quick-lru for caching

--- src/routes/_workers/blurhash.js
+++ src/routes/_workers/blurhash.js
@@ -1,11 +1,21 @@
 import { decode as decodeBlurHash } from 'blurhash'
+import QuickLRU from 'quick-lru'
 
 const RESOLUTION = 32
 const OFFSCREEN_CANVAS = typeof OffscreenCanvas === 'function'
   ? new OffscreenCanvas(RESOLUTION, RESOLUTION) : null
+const CACHE = new QuickLRU({ maxSize: 100 })
 
 self.addEventListener('message', ({ data: { encoded } }) => {
   try {
+    if (CACHE.has(encoded)) {
+      if (OFFSCREEN_CANVAS) {
+        postMessage({ encoded, decoded: CACHE.get(encoded), imageData: null, error: null })
+      } else {
+        postMessage({ encoded, imageData: CACHE.get(encoded), decoded: null, error: null })
+      }
+    }
+
     const pixels = decodeBlurHash(encoded, RESOLUTION, RESOLUTION)
 
     if (pixels) {
@@ -14,11 +24,14 @@ self.addEventListener('message', ({ data: { encoded } }) => {
       if (OFFSCREEN_CANVAS) {
         OFFSCREEN_CANVAS.getContext('2d').putImageData(imageData, 0, 0)
         OFFSCREEN_CANVAS.convertToBlob().then(blob => {
-          postMessage({ encoded, decoded: URL.createObjectURL(blob), imageData: null, error: null })
+          const decoded = URL.createObjectURL(blob)
+          CACHE.set(encoded, decoded)
+          postMessage({ encoded, decoded, imageData: null, error: null })
         }).catch(error => {
           postMessage({ encoded, decoded: null, imageData: null, error })
         })
       } else {
+        CACHE.set(encoded, imageData)
         postMessage({ encoded, imageData, decoded: null, error: null })
       }
     } else {
