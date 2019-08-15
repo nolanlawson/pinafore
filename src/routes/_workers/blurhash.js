20a6ee8c3ffd67f6d23c30777329cf933f37ad01
Sorin Davidoi <sorin.davidoi@protonmail.com>
2019-08-15 09:12:17 +0300

    fix(workers/blurhash): More error handling

--- src/routes/_workers/blurhash.js
+++ src/routes/_workers/blurhash.js
@@ -15,10 +15,14 @@ self.addEventListener('message', ({ data: { encoded } }) => {
         OFFSCREEN_CANVAS.getContext('2d').putImageData(imageData, 0, 0)
         OFFSCREEN_CANVAS.convertToBlob().then(blob => {
           postMessage({ encoded, decoded: URL.createObjectURL(blob), imageData: null, error: null })
+        }).catch(error => {
+          postMessage({ encoded, decoded: null, imageData: null, error })
         })
       } else {
         postMessage({ encoded, imageData, decoded: null, error: null })
       }
+    } else {
+      postMessage({ encoded, decoded: null, imageData: null, error: new Error('decode did not return any pixels') })
     }
   } catch (error) {
     postMessage({ encoded, decoded: null, imageData: null, error })
