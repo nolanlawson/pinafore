f8180e813f275b4e649db44e62ccc62a48d4bfec
Nolan Lawson <nolan@nolanlawson.com>
2019-08-17 14:36:13 -0700

    fix: move blurhash worker operations to before status rendering (#1391)
    * fix: move blurhash worker operations to before status rendering
    
    * slight refactor
    
    * avoid sending encoded data back and forth
    
    * move cache outside worker

---
+++ src/routes/_static/blurhash.js
@@ -0,0 +1,1 @@
+export const BLURHASH_RESOLUTION = 32
--- src/routes/_utils/blurhash.js
+++ src/routes/_utils/blurhash.js
@@ -1,51 +1,49 @@
 import BlurhashWorker from 'worker-loader!../_workers/blurhash' // eslint-disable-line
+import PromiseWorker from 'promise-worker'
+import { BLURHASH_RESOLUTION as RESOLUTION } from '../_static/blurhash'
+import QuickLRU from 'quick-lru'
 
-const RESOLUTION = 32
+const CACHE = new QuickLRU({ maxSize: 100 })
+
 let worker
 let canvas
 let canvasContext2D
 
 export function init () {
-  worker = worker || new BlurhashWorker()
+  worker = worker || new PromiseWorker(new BlurhashWorker())
+}
+
+function initCanvas () {
+  if (!canvas) {
+    canvas = document.createElement('canvas')
+    canvas.height = RESOLUTION
+    canvas.width = RESOLUTION
+    canvasContext2D = canvas.getContext('2d')
+  }
+}
+
+// canvas is the backup if we can't use OffscreenCanvas
+async function decodeUsingCanvas (imageData) {
+  initCanvas()
+  canvasContext2D.putImageData(imageData, 0, 0)
+  const blob = await new Promise(resolve => canvas.toBlob(resolve))
+  return URL.createObjectURL(blob)
+}
+
+async function decodeWithoutCache (blurhash) {
+  init()
+  const { decoded, imageData } = await worker.postMessage(blurhash)
+  if (decoded) {
+    return decoded
+  }
+  return decodeUsingCanvas(imageData)
 }
 
 export async function decode (blurhash) {
-  return new Promise((resolve, reject) => {
-    try {
-      init()
-
-      const onMessage = ({ data: { encoded, decoded, imageData, error } }) => {
-        if (encoded !== blurhash) {
-          return
-        }
-
-        worker.removeEventListener('message', onMessage)
-
-        if (error) {
-          return reject(error)
-        }
-
-        if (decoded) {
-          resolve(decoded)
-        } else {
-          if (!canvas) {
-            canvas = document.createElement('canvas')
-            canvas.height = RESOLUTION
-            canvas.width = RESOLUTION
-            canvasContext2D = canvas.getContext('2d')
-          }
-
-          canvasContext2D.putImageData(imageData, 0, 0)
-          canvas.toBlob(blob => {
-            resolve(URL.createObjectURL(blob))
-          })
-        }
-      }
-
-      worker.addEventListener('message', onMessage)
-      worker.postMessage({ encoded: blurhash })
-    } catch (e) {
-      reject(e)
-    }
-  })
+  let result = CACHE.get(blurhash)
+  if (!result) {
+    result = await decodeWithoutCache(blurhash)
+    CACHE.set(blurhash, result)
+  }
+  return result
 }
