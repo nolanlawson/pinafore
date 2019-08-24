b7c97118e2204887c98b660e253737320fb2d50c
Nolan Lawson <nolan@nolanlawson.com>
2019-08-23 20:46:21 -0700

    chore: update travis to ubuntu bionic (#1426)
    * chore: update travis to ubuntu xenial/bionic
    
    * fix apt packages
    
    * try again
    
    * fixup
    
    * fixup
    
    * fixup
    
    * fixup
    
    * fixup
    
    * simplify to test
    
    * fixup
    
    * fixup
    
    * add stuff back
    
    * add chrome
    
    * fix config
    
    * fixup
    
    * fix build script

--- bin/setup-mastodon-in-travis.sh
+++ bin/setup-mastodon-in-travis.sh
@@ -11,13 +11,6 @@ source "$HOME/.rvm/scripts/rvm"
 rvm install 2.6.1
 rvm use 2.6.1
 
-# fix for redis IPv6 issue
-# https://travis-ci.community/t/trusty-environment-redis-server-not-starting-with-redis-tools-installed/650/2
-sudo sed -e 's/^bind.*/bind 127.0.0.1/' /etc/redis/redis.conf > redis.conf
-sudo mv redis.conf /etc/redis
-sudo service redis-server start
-echo PING | nc localhost 6379 # check redis running
-
 # check versions
 ruby --version
 node --version
