72e8b8517d4bd021cd886448a9e2533a6ccdecee
Nolan Lawson <nolan@nolanlawson.com>
2019-08-27 21:08:53 -0700

    fix: add progress bar for OCR

--- src/routes/_utils/runTesseract.js
+++ src/routes/_utils/runTesseract.js
@@ -1,6 +1,25 @@
 import { importTesseractWorker } from '../_utils/asyncModules'
 
-export async function runTesseract (url) {
+// TODO: it's flaky to try to estimate tesseract's total progress this way
+const steps = [
+  { status: 'loading tesseract core', proportion: 0.05 },
+  { status: 'initializing tesseract', proportion: 0.05 },
+  { status: 'loading language traineddata', proportion: 0.1 },
+  { status: 'initializing api', proportion: 0.2 },
+  { status: 'recognizing text', proportion: 0.6 }
+]
+
+function getTotalProgress (progressInfo) {
+  const idx = steps.findIndex(({ status }) => progressInfo.status === status)
+  let total = 0
+  for (let i = 0; i < idx; i++) {
+    total += steps[i].proportion
+  }
+  total += steps[idx].proportion * progressInfo.progress
+  return total
+}
+
+export async function runTesseract (url, onProgress) {
   const worker = await importTesseractWorker()
 
   // TODO: have to trick tesseract into not creating a blob URL because that would break our CSP
@@ -13,7 +32,12 @@ export async function runTesseract (url) {
   } finally {
     window.Blob = OldBlob
   }
-  promise.progress(_ => console.log('progress', _))
+  promise.progress(progressInfo => {
+    console.log('progress', progressInfo)
+    if (onProgress && steps.find(({ status }) => status === progressInfo.status)) {
+      onProgress(getTotalProgress(progressInfo))
+    }
+  })
   const res = await promise
   return res.text
 }
