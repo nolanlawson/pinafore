26a036259e006c353783ccf987ed89f3dba9f886
Nolan Lawson <nolan@nolanlawson.com>
2019-09-07 20:27:47 -0700

    chore: update standard (#1466)

--- src/routes/_utils/asyncModules.js
+++ src/routes/_utils/asyncModules.js
@@ -2,52 +2,52 @@ const getDefault = mod => mod.default
 
 export const importTimeline = () => import(
   /* webpackChunkName: 'Timeline' */ '../_components/timeline/Timeline.html'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importVirtualList = () => import(
   /* webpackChunkName: 'VirtualList.html' */ '../_components/virtualList/VirtualList.html'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importList = () => import(
   /* webpackChunkName: 'List.html' */ '../_components/list/List.html'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importStatusVirtualListItem = () => import(
   /* webpackChunkName: 'StatusVirtualListItem.html' */ '../_components/timeline/StatusVirtualListItem.html'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importNotificationVirtualListItem = () => import(
   /* webpackChunkName: 'NotificationVirtualListItem.html' */ '../_components/timeline/NotificationVirtualListItem.html'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importDatabase = () => import(
   /* webpackChunkName: 'database.js' */ '../_database/databaseApis.js'
-  )
+)
 
 export const importLoggedInObservers = () => import(
   /* webpackChunkName: 'loggedInObservers.js' */ '../_store/observers/loggedInObservers.js'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importNavShortcuts = () => import(
-    /* webpackChunkName: 'NavShortcuts' */ '../_components/NavShortcuts.html'
-  ).then(getDefault)
+  /* webpackChunkName: 'NavShortcuts' */ '../_components/NavShortcuts.html'
+).then(getDefault)
 
 export const importEmojiMart = () => import(
   /* webpackChunkName: 'createEmojiMartPickerFromData.js' */ '../_react/createEmojiMartPickerFromData.js'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importToast = () => import(
   /* webpackChunkName: 'Toast.html' */ '../_components/toast/Toast.html'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importSnackbar = () => import(
   /* webpackChunkName: 'Snackbar.html' */ '../_components/snackbar/Snackbar.html'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importComposeBox = () => import(
   /* webpackChunkName: 'ComposeBox.html' */ '../_components/compose/ComposeBox.html'
-  ).then(getDefault)
+).then(getDefault)
 
 export const importTesseractWorker = () => import(
   /* webpackChunkName: 'tesseractWorker' */ '../_utils/tesseractWorker.js'
-  ).then(getDefault)
+).then(getDefault)
--- src/routes/_utils/asyncPolyfills.js
+++ src/routes/_utils/asyncPolyfills.js
@@ -1,15 +1,15 @@
 export const importIntersectionObserver = () => import(
   /* webpackChunkName: '$polyfill$-intersection-observer' */ 'intersection-observer'
-  )
+)
 
 export const importRequestIdleCallback = () => import(
   /* webpackChunkName: '$polyfill$-requestidlecallback' */ 'requestidlecallback'
-  )
+)
 
 export const importIndexedDBGetAllShim = () => import(
   /* webpackChunkName: '$polyfill$-indexeddb-getall-shim' */ 'indexeddb-getall-shim'
-  )
+)
 
 export const importCustomElementsPolyfill = () => import(
   /* webpackChunkName: '$polyfill$-@webcomponents/custom-elements' */ '@webcomponents/custom-elements'
-  )
+)
