29ce3e5f92acef291ab571e38fe6f8172d9692e0
Nolan Lawson <nolan@nolanlawson.com>
2019-09-19 08:54:30 -0700

    add keys

--- src/client.js
+++ src/client.js
@@ -12,6 +12,47 @@ loadPolyfills().then(() => {
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 
+console.log('hello kaios')
+
+function focusPrevious () {
+  console.log('focusPrevious')
+  const focusable = getFocusableElements()
+  const index = focusable.indexOf(document.activeElement)
+  if (index === -1) {
+    focusable[focusable.length - 1].focus()
+  } else {
+    (focusable[index - 1] || focusable[focusable.length - 1]).focus()
+  }
+}
+
+function focusNext () {
+  console.log('focusNext')
+  const focusable = getFocusableElements()
+  const index = focusable.indexOf(document.activeElement)
+  if (index === -1) {
+    focusable[0].focus()
+  } else {
+    (focusable[index + 1] || focusable[0]).focus()
+  }
+}
+
+function getFocusableElements () {
+  const query = 'a:not([disabled]), button:not([disabled]), ' +
+    'input[type=text]:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"])'
+  return Array.from(document.querySelectorAll(query)).filter(element => {
+    // check for visibility while always include the current activeElement
+    return element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement
+  })
+}
+
+window.addEventListener('keydown', e => {
+  if (e.key === 'ArrowRight') {
+    focusNext()
+  } else if (e.key === 'ArrowLeft') {
+    focusPrevious()
+  }
+})
+
 if (module.hot) {
   module.hot.accept()
 }
