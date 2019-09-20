462eaab4c376b158f24b2f95151666d5ffb2884f
Nolan Lawson <nolan@nolanlawson.com>
2019-09-19 22:30:33 -0700

    fix svgs

--- webpack/shared.config.js
+++ webpack/shared.config.js
@@ -1,6 +1,21 @@
 const svgs = require('../bin/svgs')
+const fs = require('fs')
+const path = require('path')
+const $ = require('cheerio')
 
 const inlineSvgs = svgs.filter(_ => _.inline).map(_ => `#${_.id}`)
+const allSvgs = {}
+const $inlineHtml = $(fs.readFileSync(path.join(__dirname, '../src/template.html'), 'utf8'))
+const $externalSvgs = $(fs.readFileSync(path.join(__dirname, '../static/icons.svg'), 'utf8'))
+svgs.forEach(_ => {
+  const $inlineSvg = $inlineHtml.find(`#${_.id}`)
+  const $svg = $inlineSvg.length ? $inlineSvg : $externalSvgs.find(`#${_.id}`)
+
+  allSvgs[`#${_.id}`] = {
+    viewBox: $svg.attr('viewBox'),
+    html: $svg.html()
+  }
+})
 const mode = process.env.NODE_ENV || 'production'
 const dev = mode === 'development'
 
@@ -17,5 +32,6 @@ module.exports = {
   mode,
   dev,
   resolve,
-  inlineSvgs
+  inlineSvgs,
+  allSvgs
 }
