d976fdc567847b8bdea73dc852c2f6c62cf5c7cb
Nolan Lawson <nolan@nolanlawson.com>
2019-09-20 22:09:31 -0700

    fix: improve style on very small mobile screens

--- src/routes/_store/observers/resizeObservers.js
+++ src/routes/_store/observers/resizeObservers.js
@@ -9,7 +9,8 @@ export function resizeObservers (store) {
     store.set({
       isMobileSize: matchMedia('(max-width: 767px)').matches, //  e.g. iPhone Plus
       isSmallMobileSize: matchMedia('(max-width: 479px)').matches, // e.g. Galaxy S5
-      isTinyMobileSize: matchMedia('(max-width: 320px)').matches // e.g. iPhone 4
+      isTinyMobileSize: matchMedia('(max-width: 320px)').matches, // e.g. iPhone 4
+      isVeryTinyMobileSize: matchMedia('(max-width: 240px)').matches // e.g. Nokia 8110 (KaiOS)
     })
   }
 
