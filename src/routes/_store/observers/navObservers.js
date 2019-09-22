3490f9820816b46e0b6c264941f746f2421843e6
Nolan Lawson <nolan@nolanlawson.com>
2019-09-22 16:20:26 -0700

    fix: fix nav animation for notifications/mentions (#1515)

--- src/routes/_store/observers/navObservers.js
+++ src/routes/_store/observers/navObservers.js
@@ -3,10 +3,18 @@ import { emit } from '../../_utils/eventBus'
 export function navObservers (store) {
   function pageIsInNav (store, page) {
     const { navPages } = store.get()
-    return !!navPages.find(_ => _.name === page)
+    return navPages.find(_ => _.name === page)
+  }
+
+  function normalizePageName (page) {
+    // notifications/mentions are a special case; they show as selected in the nav
+    return page === 'notifications/mentions' ? 'notifications' : page
   }
 
   store.observe('currentPage', (currentPage, previousPage) => {
+    currentPage = normalizePageName(currentPage)
+    previousPage = normalizePageName(previousPage)
+
     if (currentPage && previousPage &&
         pageIsInNav(store, currentPage) &&
         pageIsInNav(store, previousPage)) {
