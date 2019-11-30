188b913855a9b5c7633151fe6f05c8d615c67588
Nolan Lawson <nolan@nolanlawson.com>
2019-11-29 16:30:32 -0800

    refactor: refactor focus management

--- src/routes/_components/list/listStore.js
+++ src/routes/_components/list/listStore.js
@@ -1,8 +1,9 @@
 import { RealmStore } from '../../_utils/RealmStore'
+import { PAGE_HISTORY_SIZE } from '../../_static/pages'
 
 class ListStore extends RealmStore {
   constructor (state) {
-    super(state, /* maxSize */ 10)
+    super(state, /* maxSize */ PAGE_HISTORY_SIZE)
   }
 }
 
