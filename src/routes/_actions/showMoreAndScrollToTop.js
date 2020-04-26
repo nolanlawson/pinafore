559a741a26495713deabd686914fc5eb34d4a6e8
Nolan Lawson <nolan@nolanlawson.com>
2020-04-26 16:02:34 -0700

    fix focus restoration

--- src/routes/_actions/showMoreAndScrollToTop.js
+++ src/routes/_actions/showMoreAndScrollToTop.js
@@ -1,11 +1,8 @@
 import { showMoreItemsForCurrentTimeline } from './timeline'
 import { scrollToTop } from '../_utils/scrollToTop'
-import { scheduleIdleTask } from '../_utils/scheduleIdleTask'
 import { createStatusOrNotificationUuid } from '../_utils/createStatusOrNotificationUuid'
 import { store } from '../_store/store'
-
-const RETRIES = 5
-const TIMEOUT = 50
+import { tryToFocusElement } from '../_utils/tryToFocusElement'
 
 export function showMoreAndScrollToTop () {
   // Similar to Twitter, pressing "." will click the "show more" button and select
@@ -24,25 +21,9 @@ export function showMoreAndScrollToTop () {
   const notificationId = currentTimelineType === 'notifications' && firstItemSummary.id
   const statusId = currentTimelineType !== 'notifications' && firstItemSummary.id
   scrollToTop(/* smooth */ false)
-  // try 5 times to wait for the element to be rendered and then focus it
-  let count = 0
-  const tryToFocusElement = () => {
-    const uuid = createStatusOrNotificationUuid(
-      currentInstance, currentTimelineType,
-      currentTimelineValue, notificationId, statusId
-    )
-    const element = document.getElementById(uuid)
-    if (element) {
-      try {
-        element.focus({ preventScroll: true })
-      } catch (e) {
-        console.error(e)
-      }
-    } else {
-      if (++count <= RETRIES) {
-        setTimeout(() => scheduleIdleTask(tryToFocusElement), TIMEOUT)
-      }
-    }
-  }
-  scheduleIdleTask(tryToFocusElement)
+  const id = createStatusOrNotificationUuid(
+    currentInstance, currentTimelineType,
+    currentTimelineValue, notificationId, statusId
+  )
+  tryToFocusElement(id)
 }
