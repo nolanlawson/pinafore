5348b3ccce4563ee7e704698922eeafe8f029ce2
Nolan Lawson <nolan@nolanlawson.com>
2020-05-02 19:58:58 -0700

    test: do not write to mastodon.log in travis (#1761)
    * test: do not write to mastodon.log in travis
    
    * fixup

--- bin/run-mastodon.js
+++ bin/run-mastodon.js
@@ -109,7 +109,9 @@ async function runMastodon () {
     await writeFile(installedFile, '', 'utf8')
   }
   const promise = spawn('foreman', ['start'], { cwd, env })
-  const log = fs.createWriteStream('mastodon.log', { flags: 'a' })
+  // don't bother writing to mastodon.log in Travis; we can't read the file anyway
+  const logFile = process.env.TRAVIS === 'true' ? '/dev/null' : 'mastodon.log'
+  const log = fs.createWriteStream(logFile, { flags: 'a' })
   childProc = promise.childProcess
   childProc.stdout.pipe(log)
   childProc.stderr.pipe(log)
