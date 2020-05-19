28ba4823aeb7ad17d3064a9f30048330389db0b8
Nolan Lawson <nolan@nolanlawson.com>
2020-05-18 19:57:59 -0700

    perf: always load focus-visible polyfill

--- src/client.js
+++ src/client.js
@@ -6,6 +6,13 @@ import './routes/_utils/historyEvents'
 import './routes/_utils/loadingMask'
 import './routes/_utils/forceOnline'
 
+// TODO: when some browser supports :focus-visible, feature-detect and async load polyfill
+// Firefox: https://bugzilla.mozilla.org/show_bug.cgi?id=1445482
+// WebKit: https://bugs.webkit.org/show_bug.cgi?id=185859
+// Chrome: https://bugs.chromium.org/p/chromium/issues/detail?id=817199
+// Chrome ITS: https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/XKNtAVyO4AY/ujOrvaYsBwAJ
+import 'focus-visible'
+
 loadPolyfills().then(() => {
   console.log('init()')
   sapper.start({ target: document.querySelector('#sapper') })
