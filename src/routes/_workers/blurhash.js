332ff7668fcfd59abe7a9a34b4135cdae621a4a0
Nolan Lawson <nolan@nolanlawson.com>
2020-05-18 19:34:47 -0700

    perf: use OffscreenCanvas in Chrome 82+

--- src/routes/_workers/blurhash.js
+++ src/routes/_workers/blurhash.js
@@ -2,11 +2,11 @@ import '../_thirdparty/regenerator-runtime/runtime.js'
 import { decode as decodeBlurHash } from 'blurhash'
 import registerPromiseWorker from 'promise-worker/register'
 import { BLURHASH_RESOLUTION as RESOLUTION } from '../_static/blurhash'
-import { isChrome } from '../_utils/userAgent/isChrome'
+import { isChromePre82 } from '../_utils/userAgent/isChromePre82'
 
 // Disabled in Chrome because convertToBlob() is slow
 // https://github.com/nolanlawson/pinafore/issues/1396
-const OFFSCREEN_CANVAS = !isChrome() && typeof OffscreenCanvas === 'function'
+const OFFSCREEN_CANVAS = !isChromePre82() && typeof OffscreenCanvas === 'function'
   ? new OffscreenCanvas(RESOLUTION, RESOLUTION) : null
 const OFFSCREEN_CANVAS_CONTEXT_2D = OFFSCREEN_CANVAS
   ? OFFSCREEN_CANVAS.getContext('2d') : null
