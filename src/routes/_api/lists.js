36cf9fd56d5db70e562b775509236d29eeaf1c3d
Nolan Lawson <nolan@nolanlawson.com>
2020-08-25 22:16:14 -0700

    test: add test for accessible radio buttons in /community (#1845)

--- src/routes/_api/lists.js
+++ src/routes/_api/lists.js
@@ -1,7 +1,12 @@
-import { get, DEFAULT_TIMEOUT } from '../_utils/ajax'
+import { get, DEFAULT_TIMEOUT, post, WRITE_TIMEOUT } from '../_utils/ajax'
 import { auth, basename } from './utils'
 
 export function getLists (instanceName, accessToken) {
   const url = `${basename(instanceName)}/api/v1/lists`
   return get(url, auth(accessToken), { timeout: DEFAULT_TIMEOUT })
+}
+
+export function createList (instanceName, accessToken, title) {
+  const url = `${basename(instanceName)}/api/v1/lists`
+  return post(url, { title }, auth(accessToken), { timeout: WRITE_TIMEOUT })
 }
