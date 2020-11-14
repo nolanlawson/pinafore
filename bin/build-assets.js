57b75ade1bd07f424904873abc2b6aae367a3f4f
Nolan Lawson <nolan@nolanlawson.com>
2020-11-14 14:13:22 -0800

    chore: update emoji-picker-element to v1.3.0 (#1888)

--- bin/build-assets.js
+++ bin/build-assets.js
@@ -1,17 +1,15 @@
 import path from 'path'
 import fs from 'fs'
 import { promisify } from 'util'
-import trimEmojiData from 'emoji-picker-element/trimEmojiData.cjs'
 
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 
 async function main () {
-  let json = JSON.parse(await readFile(
-    path.resolve(__dirname, '../node_modules/emojibase-data/en/data.json'),
+  const json = JSON.parse(await readFile(
+    path.resolve(__dirname, '../node_modules/emoji-picker-element-data/en/emojibase-legacy/data.json'),
     'utf8')
   )
-  json = trimEmojiData(json)
   await writeFile(
     path.resolve(__dirname, '../static/emoji-all-en.json'),
     JSON.stringify(json),
