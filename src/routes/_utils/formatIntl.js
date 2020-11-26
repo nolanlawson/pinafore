1287dfcbc49d9736a9efce7e930b408ab90a37ee
Nolan Lawson <nolan@nolanlawson.com>
2020-11-25 16:44:31 -0800

    fix whitespace

--- src/routes/_utils/formatIntl.js
+++ src/routes/_utils/formatIntl.js
@@ -2,5 +2,5 @@ import interpret from 'format-message-interpret'
 import { LOCALE } from '../_static/intl'
 
 export function formatIntl (ast, values) {
-  return interpret(ast, LOCALE)(values).trim()
+  return interpret(ast, LOCALE)(values).trim().replace(/\s+/g, ' ')
 }
