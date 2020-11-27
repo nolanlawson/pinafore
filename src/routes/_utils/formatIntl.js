df431254c13f6d626744ffea90bce5e27d920e05
Nolan Lawson <nolan@nolanlawson.com>
2020-11-26 20:19:45 -0800

    more intl

--- src/routes/_utils/formatIntl.js
+++ src/routes/_utils/formatIntl.js
@@ -1,6 +1,23 @@
 import interpret from 'format-message-interpret'
 import { LOCALE } from '../_static/intl'
 
-export function formatIntl (ast, values) {
+function doFormatIntl (ast, values) {
   return interpret(ast, LOCALE)(values).trim().replace(/\s+/g, ' ')
+}
+
+export function formatIntl (ast, values) {
+  if (process.env.NODE_ENV !== 'production') {
+    // useful error debugging for dev mode
+    if (typeof ast === 'string') {
+      throw new Error('bad ast: ' + ast)
+    }
+    try {
+      return doFormatIntl(ast, values)
+    } catch (err) {
+      console.error(err)
+      throw new Error('bad ast or values : ' + ast + ' ' + values)
+    }
+  }
+
+  return doFormatIntl(ast, values)
 }
