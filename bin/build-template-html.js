b7409225a10979b25c621682cc9237cad6327269
Nolan Lawson <nolan@nolanlawson.com>
2020-11-27 10:07:22 -0800

    add ltr/rtl

--- bin/build-template-html.js
+++ bin/build-template-html.js
@@ -9,9 +9,10 @@ import now from 'performance-now'
 import debounce from 'lodash-es/debounce'
 import applyIntl from '../webpack/svelte-intl-loader'
 import { LOCALE } from '../src/routes/_static/intl'
+import { getLangDir } from 'rtl-detect'
 
 const writeFile = promisify(fs.writeFile)
-
+const LOCALE_DIRECTION = getLangDir(LOCALE)
 const DEBOUNCE = 500
 
 const builders = [
@@ -92,6 +93,7 @@ async function buildAll () {
 
   html = applyIntl(html)
     .replace('{process.env.LOCALE}', LOCALE)
+    .replace('{process.env.LOCALE_DIRECTION}', LOCALE_DIRECTION)
   await writeFile(path.resolve(__dirname, '../src/template.html'), html, 'utf8')
   const end = now()
   console.log(`Built template.html in ${(end - start).toFixed(2)}ms`)
