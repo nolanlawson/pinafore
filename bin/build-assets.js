ff8ce8565da28c5543b1a4570d9fd244b1583b11
Nolan Lawson <nolan@nolanlawson.com>
2020-12-13 17:41:40 -0800

    feat: intl support for emoji picker
    Fixes #1908

--- bin/build-assets.js
+++ bin/build-assets.js
@@ -1,17 +1,48 @@
 import path from 'path'
 import fs from 'fs'
 import { promisify } from 'util'
+import { LOCALE } from '../src/routes/_static/intl'
 
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 
+// Try 'en-US' first, then 'en' if that doesn't exist
+const PREFERRED_LOCALES = [LOCALE, LOCALE.split('-')[0]]
+
+// emojibase seems like the most "neutral" shortcodes, but cldr is available in every language
+const PREFERRED_SHORTCODES = ['emojibase', 'cldr']
+
+async function getEmojiI18nFile (locale, shortcode) {
+  const filename = path.resolve(__dirname,
+    '../node_modules/emoji-picker-element-data',
+    locale,
+    shortcode,
+    'data.json')
+  try {
+    return JSON.parse(await readFile(filename, 'utf8'))
+  } catch (err) { /* ignore */ }
+}
+
+async function getFirstExistingEmojiI18nFile () {
+  for (const locale of PREFERRED_LOCALES) {
+    for (const shortcode of PREFERRED_SHORTCODES) {
+      const json = await getEmojiI18nFile(locale, shortcode)
+      if (json) {
+        return json
+      }
+    }
+  }
+}
+
 async function main () {
-  const json = JSON.parse(await readFile(
-    path.resolve(__dirname, '../node_modules/emoji-picker-element-data/en/emojibase-legacy/data.json'),
-    'utf8')
-  )
+  const json = await getFirstExistingEmojiI18nFile()
+
+  if (!json) {
+    throw new Error(`Couldn't find i18n data for locale ${LOCALE}. Is it supported in emoji-picker-element-data?`)
+  }
+
   await writeFile(
-    path.resolve(__dirname, '../static/emoji-all-en.json'),
+    path.resolve(__dirname, `../static/emoji-${LOCALE}.json`),
     JSON.stringify(json),
     'utf8'
   )
