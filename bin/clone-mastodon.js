3be34f805672c5a7e345d93cf0d4e3af18c20843
Nolan Lawson <nolan@nolanlawson.com>
2021-01-24 13:27:02 -0800

    test: use ruby 2.6.6

--- bin/clone-mastodon.js
+++ bin/clone-mastodon.js
@@ -2,7 +2,7 @@ import { promisify } from 'util'
 import childProcessPromise from 'child-process-promise'
 import path from 'path'
 import fs from 'fs'
-import { envFile } from './mastodon-config'
+import { envFile, RUBY_VERSION } from './mastodon-config'
 
 const exec = childProcessPromise.exec
 const stat = promisify(fs.stat)
@@ -21,6 +21,7 @@ export default async function cloneMastodon () {
     console.log('Cloning mastodon...')
     await exec(`git clone --single-branch --branch ${GIT_TAG} ${GIT_URL} "${mastodonDir}"`)
     await writeFile(path.join(dir, '../mastodon/.env'), envFile, 'utf8')
+    await writeFile(path.join(dir, '../mastodon/.ruby-version'), RUBY_VERSION, 'utf8')
   }
 }
 
