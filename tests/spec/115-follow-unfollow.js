976b556fcd63e63e7cf1fe331c660b2df6a2a41f
Nolan Lawson <nolan@nolanlawson.com>
2021-02-15 16:17:23 -0800

    test: fix flakey test

--- tests/spec/115-follow-unfollow.js
+++ tests/spec/115-follow-unfollow.js
@@ -1,7 +1,8 @@
 import {
   accountProfileFollowButton,
   accountProfileMoreOptionsButton, closeDialogButton,
-  getNthDialogOptionsOption
+  getNthDialogOptionsOption,
+  sleep
 } from '../utils'
 import { loginAsFoobar } from '../roles'
 import { unfollowAs } from '../serverActions'
@@ -16,19 +17,33 @@ test('Can follow and unfollow an account from the profile page', async t => {
     .navigateTo('/accounts/5')
     .expect(accountProfileFollowButton.getAttribute('aria-label')).eql('Follow')
     .expect(accountProfileFollowButton.getAttribute('title')).eql('Follow')
+  await sleep(500)
+  await t
     .click(accountProfileMoreOptionsButton)
+  await sleep(500)
+  await t
     .expect(getNthDialogOptionsOption(1).innerText).contains('Mention @baz')
     .expect(getNthDialogOptionsOption(2).innerText).contains('Follow @baz')
+  await sleep(500)
+  await t
     .click(getNthDialogOptionsOption(2))
     .expect(accountProfileFollowButton.getAttribute('aria-label')).eql('Unfollow')
     .expect(accountProfileFollowButton.getAttribute('title')).eql('Unfollow')
+  await sleep(500)
+  await t
     .click(accountProfileMoreOptionsButton)
     .expect(getNthDialogOptionsOption(2).innerText).contains('Unfollow @baz')
+  await sleep(500)
+  await t
     .click(getNthDialogOptionsOption(2))
     .expect(accountProfileFollowButton.getAttribute('aria-label')).eql('Follow')
     .expect(accountProfileFollowButton.getAttribute('title')).eql('Follow')
+  await sleep(500)
+  await t
     .click(accountProfileMoreOptionsButton)
     .expect(getNthDialogOptionsOption(2).innerText).contains('Follow @baz')
+  await sleep(500)
+  await t
     .click(closeDialogButton)
     .expect(accountProfileFollowButton.getAttribute('aria-label')).eql('Follow')
     .expect(accountProfileFollowButton.getAttribute('title')).eql('Follow')
