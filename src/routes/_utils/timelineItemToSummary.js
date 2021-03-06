ac0e9689624645d210ec6d47e139babb3a6d14f2
Nolan Lawson <nolan@nolanlawson.com>
2021-03-05 22:14:03 -0800

    fix: use class instead of object for easier debugging

--- src/routes/_utils/timelineItemToSummary.js
+++ src/routes/_utils/timelineItemToSummary.js
@@ -1,9 +1,13 @@
-export function timelineItemToSummary (item) {
-  return {
-    id: item.id,
-    accountId: item.account.id,
-    replyId: (item.in_reply_to_id) || undefined,
-    reblogId: (item.reblog && item.reblog.id) || undefined,
-    type: item.type || undefined
+class TimelineSummary {
+  constructor (item) {
+    this.id = item.id
+    this.accountId = item.account.id
+    this.replyId = (item.in_reply_to_id) || undefined
+    this.reblogId = (item.reblog && item.reblog.id) || undefined
+    this.type = item.type || undefined
   }
+}
+
+export function timelineItemToSummary (item) {
+  return new TimelineSummary(item)
 }
