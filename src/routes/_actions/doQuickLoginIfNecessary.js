60849132951b9928f03101c35844406e48306baa
Nolan Lawson <nolan@nolanlawson.com>
2021-03-14 11:06:32 -0700

    perf: remove quick-login.html

---
+++ src/routes/_actions/doQuickLoginIfNecessary.js
@@ -0,0 +1,35 @@
+// "Secret" API to quickly log in with an access token and instance name.
+// Used in the integration tests. Can't see a problem with exposing this publicly
+// since you would have to know the access token anyway.
+
+import { store } from '../_store/store'
+import { goto } from '../../../__sapper__/client'
+
+export function doQuickLoginIfNecessary () {
+  const params = new URLSearchParams(location.search)
+  const accessToken = params.get('accessToken')
+  const instanceName = params.get('instanceName')
+  if (!accessToken || !instanceName) {
+    return
+  }
+  const {
+    loggedInInstances,
+    loggedInInstancesInOrder
+  } = store.get()
+
+  loggedInInstances[instanceName] = {
+    access_token: accessToken
+  }
+
+  if (!loggedInInstancesInOrder.includes(instanceName)) {
+    loggedInInstancesInOrder.push(instanceName)
+  }
+
+  store.set({
+    currentInstance: instanceName,
+    loggedInInstances,
+    loggedInInstancesInOrder
+  })
+  store.save()
+  goto('/') // re-navigate without the URL params
+}
