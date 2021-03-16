205e9cd7a7055ee5b23fddecabe80722c368d2da
Nolan Lawson <nolan@nolanlawson.com>
2021-03-15 19:16:53 -0700

    fix: fix name of webpack chunk for intl polyfill

--- src/routes/_utils/polyfills/asyncPolyfills.js
+++ src/routes/_utils/polyfills/asyncPolyfills.js
@@ -7,10 +7,33 @@ export const importFocusVisible = () => import(
   /* webpackChunkName: '$polyfill$-focus-visible' */ 'focus-visible'
 )
 
-export const importRelativeTimeFormat = () => import(
-  /* webpackChunkName: '$polyfill$-relative-time-format' */ './relativeTimeFormatPolyfill'
+export const importIntlLocale = () => import(
+  /* webpackChunkName: '$polyfill$-internationalization' */ '@formatjs/intl-locale/polyfill'
 )
 
-export const importListFormat = () => import(
-  /* webpackChunkName: '$polyfill$-list-format' */ './listFormatPolyfill'
-)
+export const importIntlPluralRules = async () => { // has to be imported serially
+  await import(
+    /* webpackChunkName: '$polyfill$-internationalization' */ '@formatjs/intl-pluralrules/polyfill'
+  )
+  await import(
+    /* webpackChunkName: '$polyfill$-internationalization' */ '@formatjs/intl-pluralrules/locale-data/en'
+  )
+}
+
+export const importIntlRelativeTimeFormat = async () => { // has to be imported serially
+  await import(
+    /* webpackChunkName: '$polyfill$-internationalization' */ '@formatjs/intl-relativetimeformat/polyfill'
+  )
+  await import(
+    /* webpackChunkName: '$polyfill$-internationalization' */ '@formatjs/intl-relativetimeformat/locale-data/en'
+  )
+}
+
+export const importIntlListFormat = async () => { // has to be imported serially
+  await import(
+    /* webpackChunkName: '$polyfill$-internationalization' */ '@formatjs/intl-listformat/polyfill'
+  )
+  await import(
+    /* webpackChunkName: '$polyfill$-internationalization' */ '@formatjs/intl-listformat/locale-data/en'
+  )
+}
