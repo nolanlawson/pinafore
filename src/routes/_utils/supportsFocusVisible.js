1882613c678200467ea6aecc385ffb9817920b71
Nolan Lawson <nolan@nolanlawson.com>
2021-03-17 20:32:19 -0700

    fix: use unambiguous closure

--- src/routes/_utils/supportsFocusVisible.js
+++ src/routes/_utils/supportsFocusVisible.js
@@ -4,4 +4,4 @@ import { isFirefox } from './userAgent/isFirefox'
 
 // TODO: remove the Firefox check once this bug is fixed
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1699154
-export const supportsFocusVisible = thunk(() => !isFirefox() && supportsSelector(':focus-visible'))
+export const supportsFocusVisible = thunk(() => (!isFirefox() && supportsSelector(':focus-visible')))
