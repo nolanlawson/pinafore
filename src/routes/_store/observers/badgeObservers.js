9585f20d6b2d946b0f7fec1a2177c13c6e49223d
Nolan Lawson <nolan@nolanlawson.com>
2021-03-19 07:25:21 -0700

    fix: tweak

--- src/routes/_store/observers/badgeObservers.js
+++ src/routes/_store/observers/badgeObservers.js
@@ -5,7 +5,7 @@ export function badgeObservers () {
   if (!process.browser) {
     return
   }
-  // Chrome 86 on Linux in Circle CI seems to throw an error just by checking for this... not worth supporting.
+  // Chrome 86 on Linux in Circle CI seems to hang just by checking for this... not worth supporting.
   if (isChromePre87() || !('setAppBadge' in navigator)) {
     return
   }
--- src/routes/_utils/userAgent/isChromePre87.js
+++ src/routes/_utils/userAgent/isChromePre87.js
@@ -2,4 +2,4 @@ import { isChrome } from './isChrome'
 import { thunk } from '../thunk'
 
 // https://caniuse.com/cookie-store-api
-export const isChromePre87 = thunk(() => process.browser && isChrome() && typeof cookieStore === 'undefined')
+export const isChromePre87 = thunk(() => (process.browser && isChrome() && typeof cookieStore === 'undefined'))
