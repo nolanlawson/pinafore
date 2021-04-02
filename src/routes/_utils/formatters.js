1416fa207b84a62b942011c478fc1136efa3bd94
Nolan Lawson <nolan@nolanlawson.com>
2021-04-02 08:45:44 -0700

    fix: initialize all Intl formatters lazily
    fixes #2024

--- src/routes/_utils/formatters.js
+++ src/routes/_utils/formatters.js
@@ -1,23 +1,24 @@
 import { LOCALE } from '../_static/intl'
+import { thunk } from './thunk'
 
-export const absoluteDateFormatter = new Intl.DateTimeFormat(LOCALE, {
+export const absoluteDateFormatter = thunk(() => new Intl.DateTimeFormat(LOCALE, {
   year: 'numeric',
   month: 'long',
   day: 'numeric',
   hour: '2-digit',
   minute: '2-digit'
-})
+}))
 
-export const shortAbsoluteDateFormatter = new Intl.DateTimeFormat(LOCALE, {
+export const shortAbsoluteDateFormatter = thunk(() => new Intl.DateTimeFormat(LOCALE, {
   year: 'numeric',
   month: 'short',
   day: 'numeric',
   hour: '2-digit',
   minute: '2-digit'
-})
+}))
 
-export const dayOnlyAbsoluteDateFormatter = new Intl.DateTimeFormat(LOCALE, {
+export const dayOnlyAbsoluteDateFormatter = thunk(() => new Intl.DateTimeFormat(LOCALE, {
   year: 'numeric',
   month: 'short',
   day: 'numeric'
-})
+}))
