3a539e2d69e55ab039efabd64e14b15c58609953
Nolan Lawson <nolan@nolanlawson.com>
2021-04-10 12:52:55 -0700

    fix: add code comment

--- src/routes/_utils/testEmojiSupported.js
+++ src/routes/_utils/testEmojiSupported.js
@@ -2,6 +2,7 @@ import { isEmojiSupported, setCacheHandler } from 'is-emoji-supported'
 import { QuickLRU } from '../_thirdparty/quick-lru/quick-lru'
 
 // avoid recomputing emoji support over and over again
+// use our own LRU since the built-in one grows forever, which is a small memory leak, but still
 setCacheHandler(new QuickLRU({ maxSize: 500 }))
 
 export const testEmojiSupported = isEmojiSupported
