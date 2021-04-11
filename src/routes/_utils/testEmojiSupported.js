7a28bd2d880dd228026018e22c1afda69adf9f59
Nolan Lawson <nolan@nolanlawson.com>
2021-04-11 07:42:30 -0700

    fix: use is-emoji-supported library (#2030)
    * fix: use is-emoji-supported library
    
    * fix: add code comment

--- src/routes/_utils/testEmojiSupported.js
+++ src/routes/_utils/testEmojiSupported.js
@@ -1,17 +1,8 @@
-import { testColorEmojiSupported } from './testColorEmojiSupported'
-import { testEmojiRenderedAtCorrectSize } from './testEmojiRenderedAtCorrectSize'
+import { isEmojiSupported, setCacheHandler } from 'is-emoji-supported'
 import { QuickLRU } from '../_thirdparty/quick-lru/quick-lru'
 
 // avoid recomputing emoji support over and over again
-const emojiSupportCache = new QuickLRU({
-  maxSize: 500
-})
+// use our own LRU since the built-in one grows forever, which is a small memory leak, but still
+setCacheHandler(new QuickLRU({ maxSize: 500 }))
 
-export function testEmojiSupported (unicode) {
-  let supported = emojiSupportCache.get(unicode)
-  if (typeof supported !== 'boolean') {
-    supported = !!(testColorEmojiSupported(unicode) && testEmojiRenderedAtCorrectSize(unicode))
-    emojiSupportCache.set(unicode, supported)
-  }
-  return supported
-}
+export const testEmojiSupported = isEmojiSupported
