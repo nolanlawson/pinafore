452b3e4bd9e3c8329a1217a899251fe987873c0c
Nolan Lawson <nolan@nolanlawson.com>
2021-04-11 10:46:08 -0700

    Revert "test: fix test"
    This reverts commit 559e3d80eb8d65f7ab9443966d4fcfd087e4f8f0.

--- tests/unit/test-intl.js
+++ tests/unit/test-intl.js
@@ -16,7 +16,7 @@ describe('test-intl.js', () => {
   it('has no unused intl strings', async () => {
     const keys = Object.keys(enIntl)
 
-    const allSourceFilenames = (await globby([path.join(__dirname, '../../src/**/*.{js,html,json}')]))
+    const allSourceFilenames = (await globby([path.join(__dirname, '../../src/**/*.{js,html}')]))
       .filter(file => !file.includes('/intl/'))
     const allSourceFiles = await Promise.all(
       allSourceFilenames.map(async name => ({ name, content: await readFile(name, 'utf8') }))
