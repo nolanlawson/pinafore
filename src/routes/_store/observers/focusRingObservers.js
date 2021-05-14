890e4a087356b612ef8026860b730785c8dbd606
Nolan Lawson <nolan@nolanlawson.com>
2021-05-14 07:12:03 -0700

    fix: fully disable focus-visible for firefox for now

--- src/routes/_store/observers/focusRingObservers.js
+++ src/routes/_store/observers/focusRingObservers.js
@@ -1,9 +1,12 @@
-const style = process.browser && document.getElementById('theFocusVisibleStyle')
+import { supportsFocusVisible } from '../../_utils/supportsFocusVisible'
 
 export function focusRingObservers (store) {
   if (!process.browser) {
     return
   }
+
+  const styleId = supportsFocusVisible() ? 'theFocusVisibleStyle' : 'theFocusVisiblePolyfillStyle'
+  const style = document.getElementById(styleId)
 
   store.observe('alwaysShowFocusRing', alwaysShowFocusRing => {
     style.setAttribute('media', alwaysShowFocusRing ? 'only x' : 'all') // disable or enable the style
