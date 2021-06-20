dbf8851a8cc84972e3e74f278571a1750ae3c2a2
Nolan Lawson <nolan@nolanlawson.com>
2021-06-20 09:05:42 -0700

    fix: work around missing indexedDB.databases

---
+++ src/routes/_utils/idbReady.js
@@ -0,0 +1,14 @@
+import safariIdbReady from 'safari-14-idb-fix'
+import { isWebKit } from './userAgent/isWebKit'
+
+// workaround for a safari 14 bug, see https://github.com/jakearchibald/safari-14-idb-fix
+export async function idbReady () {
+  if (!isWebKit()) {
+    return
+  }
+  if (typeof indexedDB === 'undefined' || !indexedDB.databases) {
+    // fix for https://github.com/jakearchibald/safari-14-idb-fix/pull/2
+    return
+  }
+  await safariIdbReady()
+}
