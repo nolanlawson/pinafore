2411a5943c13077d051fdafc361126f94a6517ab
Nolan Lawson <nolan@nolanlawson.com>
2021-07-04 12:38:39 -0700

    chore: update ruby and node versions in Circle CI

--- bin/mastodon-config.js
+++ bin/mastodon-config.js
@@ -17,9 +17,7 @@ DB_NAME=${DB_NAME}
 DB_PASS=${DB_PASS}
 `
 
-// Need a Ruby version that CircleCI bundles with Node v12, not Node v14 which doesn't
-// work for streaming
-export const RUBY_VERSION = '2.6.6'
+export const RUBY_VERSION = '2.7.2'
 
 export const mastodonDir = path.join(__dirname, '../mastodon')
 
