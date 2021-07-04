b31a72f8502e030c3d72060b55a81be1cc7166d4
Nolan Lawson <nolan@nolanlawson.com>
2021-07-04 16:38:58 -0700

    fix : update deps, remove unused deps, code cleanup (#2061)

--- webpack/shared.config.js
+++ webpack/shared.config.js
@@ -1,21 +1,6 @@
 const svgs = require('../bin/svgs')
-const fs = require('fs')
-const path = require('path')
-const $ = require('cheerio')
 
 const inlineSvgs = svgs.filter(_ => _.inline).map(_ => `#${_.id}`)
-const allSvgs = {}
-const $inlineHtml = $(fs.readFileSync(path.join(__dirname, '../src/template.html'), 'utf8'))
-const $externalSvgs = $(fs.readFileSync(path.join(__dirname, '../static/icons.svg'), 'utf8'))
-svgs.forEach(_ => {
-  const $inlineSvg = $inlineHtml.find(`#${_.id}`)
-  const $svg = $inlineSvg.length ? $inlineSvg : $externalSvgs.find(`#${_.id}`)
-
-  allSvgs[`#${_.id}`] = {
-    viewBox: $svg.attr('viewBox'),
-    html: $svg.html()
-  }
-})
 const mode = process.env.NODE_ENV || 'production'
 const dev = mode === 'development'
 
@@ -34,6 +19,5 @@ module.exports = {
   mode,
   dev,
   resolve,
-  inlineSvgs,
-  allSvgs
+  inlineSvgs
 }
