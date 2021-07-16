d30f7f4b1ac9b46044222a065e4633f1ab76de1d
Nolan Lawson <nolan@nolanlawson.com>
2021-07-16 07:14:15 -0700

    fix: enable focus-visible in Firefox 90 (#2075)

--- src/routes/_utils/supportsFocusVisible.js
+++ src/routes/_utils/supportsFocusVisible.js
@@ -1,8 +1,8 @@
 import { thunk } from './thunk.js'
 import { supportsSelector } from './supportsSelector.js'
-import { isFirefox } from './userAgent/isFirefox.js'
+import { isFirefoxPre90 } from './userAgent/isFirefoxPre90.js'
 
-// Disabling for now in Firefox due to bugs:
+// Disabling in Firefox <90 due to bugs:
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1699154
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1711057
-export const supportsFocusVisible = thunk(() => (!isFirefox() && supportsSelector(':focus-visible')))
+export const supportsFocusVisible = thunk(() => (!isFirefoxPre90() && supportsSelector(':focus-visible')))
