2accc46d8f11a362e2c729f0899c6c47050f379c
Nolan Lawson <nolan@nolanlawson.com>
2022-03-27 20:35:24 -0700

    refactor: refactor

--- src/routes/_static/fonts.js
+++ src/routes/_static/fonts.js
@@ -1,6 +1,3 @@
 // same as the one used for PinaforeEmoji
 export const FONT_FAMILY = '"Twemoji Mozilla","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol",' +
   '"Noto Color Emoji","EmojiOne Color","Android Emoji",sans-serif'
-
-export const COUNTRY_FLAG_FONT_FAMILY = 'Twemoji Mozilla'
-export const COUNTRY_FLAG_FONT_URL = '/TwemojiCountryFlags.woff2'
--- src/routes/_store/observers/countryFlagEmojiPolyfill.js
+++ src/routes/_store/observers/countryFlagEmojiPolyfill.js
@@ -1,14 +1,15 @@
 import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill'
 import { store } from '../store.js'
-import { COUNTRY_FLAG_FONT_FAMILY, COUNTRY_FLAG_FONT_URL } from '../../_static/fonts.js'
 
 let polyfilled = false
+
+const COUNTRY_FLAG_FONT_URL = '/TwemojiCountryFlags.woff2'
 
 export function countryFlagEmojiPolyfill () {
   if (!polyfilled) {
     polyfilled = true
     const numStylesBefore = document.head.querySelectorAll('style').length
-    polyfillCountryFlagEmojis(COUNTRY_FLAG_FONT_FAMILY, COUNTRY_FLAG_FONT_URL)
+    polyfillCountryFlagEmojis('Twemoji Mozilla', COUNTRY_FLAG_FONT_URL)
     const numStylesAfter = document.head.querySelectorAll('style').length
     // if a style was added, then the polyfill was activated
     const polyfillActivated = numStylesAfter !== numStylesBefore
