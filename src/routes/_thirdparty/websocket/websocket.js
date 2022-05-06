a6bf816bdb577d62a6843b94d78777c2aa8001bc
tsmethurst <tobi.smethurst@protonmail.com>
2022-05-06 14:41:48 +0200

    use empty array for ws protocols instead of null

--- src/routes/_thirdparty/websocket/websocket.js
+++ src/routes/_thirdparty/websocket/websocket.js
@@ -8,7 +8,7 @@ export class WebSocketClient {
    * @param protocols DOMString|DOMString[] Either a single protocol string or an array of protocol strings. These strings are used to indicate sub-protocols, so that a single server can implement multiple WebSocket sub-protocols (for example, you might want one server to be able to handle different types of interactions depending on the specified protocol). If you don't specify a protocol string, an empty string is assumed.
    * @param options options
    */
-  constructor (url, protocols = null, options = {}) {
+  constructor (url, protocols = [], options = {}) {
     this.url = url
     this.protocols = protocols
 
