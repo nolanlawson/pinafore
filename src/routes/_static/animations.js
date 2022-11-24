3bc3ba025cadbddbba0186a7db631b70826c2fd9
Nolan Lawson <nolan@nolanlawson.com>
2022-11-24 08:37:00 -0800

    fix: transition checkmark color too

--- src/routes/_static/animations.js
+++ src/routes/_static/animations.js
@@ -1,39 +1,37 @@
-export const FAVORITE_ANIMATION = [
-  {
-    properties: [
-      { transform: 'scale(1)' },
-      { transform: 'scale(2)' },
-      { transform: 'scale(1)' }
-    ],
-    options: {
-      duration: 333,
-      easing: 'ease-in-out'
-    }
-  },
-  {
-    properties: [
-      { fill: 'var(--action-button-fill-color)' },
-      { fill: 'var(--action-button-fill-color-pressed)' }
-    ],
-    options: {
-      duration: 333,
-      easing: 'linear'
-    }
+const growBigThenSmall = {
+  properties: [
+    { transform: 'scale(1)' },
+    { transform: 'scale(2)' },
+    { transform: 'scale(1)' }
+  ],
+  options: {
+    duration: 333,
+    easing: 'ease-in-out'
   }
+}
+
+const fadeColorToPressedState = {
+  properties: [
+    { fill: 'var(--action-button-fill-color)' },
+    { fill: 'var(--action-button-fill-color-pressed)' }
+  ],
+  options: {
+    duration: 333,
+    easing: 'linear'
+  }
+}
+
+export const FAVORITE_ANIMATION = [
+  growBigThenSmall,
+  fadeColorToPressedState
 ]
 
 export const REBLOG_ANIMATION = FAVORITE_ANIMATION
 
 export const FOLLOW_BUTTON_ANIMATION = [
-  {
-    properties: [
-      { transform: 'scale(1)' },
-      { transform: 'scale(2)' },
-      { transform: 'scale(1)' }
-    ],
-    options: {
-      duration: 333,
-      easing: 'ease-in-out'
-    }
-  }
+  growBigThenSmall
+]
+
+export const CHECKMARK_ANIMATION = [
+  fadeColorToPressedState
 ]
