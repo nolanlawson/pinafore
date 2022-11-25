4ea595b7ae81692bf3fefb052b74586ebc9745c9
Nolan Lawson <nolan@nolanlawson.com>
2022-11-25 09:02:11 -0800

    chore: fix unit tests, split e2e tests

--- bin/test-vercel-json-unchanged.sh
+++ bin/test-vercel-json-unchanged.sh
@@ -3,8 +3,8 @@
 # In CI, we need to make sure the vercel.json file is built correctly,
 # or else it will mess up the deployment to Vercel
 
-if ! diff -q vercel-old.json vercel.json &>/dev/null; then
-  diff vercel-old.json vercel.json
+if ! diff -q /tmp/vercel-old.json ./vercel.json &>/dev/null; then
+  diff /tmp/vercel-old.json ./vercel.json
   echo "vercel.json changed, run yarn build and make sure everything looks okay"
   exit 1
 fi
