7cd5a6b6fe92f391403b4f4d211171d4afc70b1e
Nolan Lawson <nolan@nolanlawson.com>
2022-11-25 11:43:54 -0800

    chore: remove emoji picker i18n files

--- webpack/client.config.js
+++ webpack/client.config.js
@@ -10,8 +10,8 @@ import { version } from '../package.json'
 
 import urlRegex from '../src/routes/_utils/urlRegexSource.js'
 // TODO: make it so we don't have to list these out explicitly
-import fr from '../src/intl/emoji-picker/fr.js'
-import de from '../src/intl/emoji-picker/de.js'
+import fr from 'emoji-picker-element/i18n/fr.js'
+import de from 'emoji-picker-element/i18n/de.js'
 
 const emojiPickerLocales = { fr, de }
 
