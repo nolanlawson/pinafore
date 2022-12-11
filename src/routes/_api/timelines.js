b1dc43a9c94b5698a51093b8d3944cbe31f0daa9
Nolan Lawson <nolan@nolanlawson.com>
2022-12-11 12:01:01 -0800

    fix: show proper notification text for follow request (#2314)
    Fixes #1800

--- src/routes/_api/timelines.js
+++ src/routes/_api/timelines.js
@@ -66,7 +66,7 @@ export async function getTimeline (instanceName, accessToken, timeline, maxId, s
   }
 
   if (timeline === 'notifications/mentions') {
-    params.exclude_types = ['follow', 'favourite', 'reblog', 'poll', 'admin.sign_up']
+    params.exclude_types = ['follow', 'favourite', 'reblog', 'poll', 'admin.sign_up', 'update', 'follow_request']
   }
 
   url += '?' + paramsString(params)
